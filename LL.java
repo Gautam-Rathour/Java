c1bea3eb35d29f4cf2d5604103f7df125b991509
Gautam-Rathour <gau321tam@gmail.com>
2024-09-13 14:13:19 +0530

    Add

--- LL.java
+++ LL.java
@@ -2666,7 +2666,6 @@ public class second{
 //         }
 //         System.out.print("null");
 //     }
-// }
 
 // public static void main (String args[]) {
 //     LL list = new LL ();
@@ -2678,7 +2677,7 @@ public class second{
 //     list.addFirst("This");
 //     list.printList();
 // }
-
+// }
 
 // //==================================================================================================== 
 // //====================================================================================================
@@ -2717,7 +2716,131 @@ public class second{
 // //==================================================================================================== 
 // //====================================================================================================
 
+class LL {
+    Node head;
+    private int size;
 
+    LL () {
+        this.size = 0;
+    }
+    class Node {
+        String data;
+        Node next;
+
+        Node (String data) {
+            this.data = data;
+            this.next = null;
+            size++;
+        }
+    }
+
+    // add - first , last
+    public void addFirst(String data) {
+        Node newNode = new Node(data);
+        if(head == null) {
+            head = newNode;
+            return;
+        } 
+        newNode.next = head;
+        head = newNode;
+    }
+
+    // add last :-
+    public void addLast(String data) {
+        Node newNode = new Node(data);
+        if(head == null) {
+            head = newNode;
+            return;
+        }
+        Node currNode = head;
+        while(currNode.next != null) {
+            currNode = currNode.next;
+        }
+        currNode.next = newNode;
+    }
+
+    // print
+    public void printList() {
+        if(head == null) {
+            System.out.println("List is Empty");
+            return;
+        }
+        Node currNode = head;
+        while(currNode != null) {
+            System.out.print(currNode.data + " -> ");
+            currNode = currNode.next;
+        }
+        System.out.println("Null");
+    }
+
+    // delete First :- 
+    public void deleteFirst() {
+        if(head == null) {
+            System.out.println("The list is Empty");
+            return;
+        }
+        size--;
+        head = head.next;
+    }
+
+    // delete Last :-
+    public void deleteLast() {
+        if(head == null) {
+            System.out.println("The list is Empty");
+            return;
+        }
+
+        size--;
+        if(head.next == null) {
+            head = null;
+            return;
+        }
+
+        Node secondLast = head;
+        Node lastNode = head.next; // head.next = null -> lastNode = null
+        while(lastNode.next != null) { // null.next
+            lastNode = lastNode.next;
+            secondLast = secondLast.next;
+        }
+        secondLast.next = null;
+    }
+
+    public int getSize () {
+        return size;
+    }
+
+    public void reverseIterate() {
+        if(head == null || head.next == null) {
+            return;
+        }
+
+        Node prevNode = head;
+        Node currNode = head.next;
+        while(currNode != null) {
+            Node nextNode = currNode.next;
+            currNode.next = prevNode;
+
+            // Update :-
+            prevNode = currNode;
+            currNode = nextNode;
+        }
+        head.next = null;
+        head = prevNode;
+    }
+
+    public static void main (String args[]) {
+        LL list = new LL();
+        list.addLast("1");
+        list.addLast("2");
+        list.addLast("3");
+        list.addLast("4");
+        list.addLast("5");
+        list.printList();
+
+        list.reverseIterate();
+        list.printList();
+    }
+}
 
 
 
