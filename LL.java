ae8d52db7fc5eefc7e612ee70a681c744b79bb9a
Gautam-Rathour <gau321tam@gmail.com>
2024-09-13 14:15:42 +0530

    Delete

--- LL.java
+++ LL.java
@@ -2835,7 +2835,7 @@ class LL {
         list.addLast("3");
         list.addLast("4");
         list.addLast("5");
-        list.addLast("6");
+        list
         list.printList();
 
         list.reverseIterate();
