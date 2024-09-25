68fcd2846ff67fdd3c4139085ab16ec11b8d1343
Gautam-Rathour <gau321tam@gmail.com>
2024-09-25 14:48:35 +0530

    Add

--- stackclass.java
+++ stackclass.java
@@ -3211,62 +3211,62 @@ public class second{
 
 // // Stacks :-
 
-// public class stackclass {
-//     static class Node {
-//         int data;
-//         Node next;
-//         public Node (int  data){
-//             this.data = data;
-//             next = null;
-//         }
-//     }
-//     static class Stack {
-//         public static Node head;
-//         public static boolean isEmpty() {
-//             return head == null;
-//         }
-//         public static void push(int data) {
-//             Node newNode = new Node (data);
-//             if(isEmpty()) {
-//                 head = newNode;
-//                 return;
-//             }
+public class stackclass {
+    static class Node {
+        int data;
+        Node next;
+        public Node (int  data){
+            this.data = data;
+            next = null;
+        }
+    }
+    static class Stack {
+        public static Node head;
+        public static boolean isEmpty() {
+            return head == null;
+        }
+        public static void push(int data) {
+            Node newNode = new Node (data);
+            if(isEmpty()) {
+                head = newNode;
+                return;
+            }
 
-//             newNode.next = head;
-//             head = newNode;
-//         }
+            newNode.next = head;
+            head = newNode;
+        }
 
-//         public static int pop() {
-//             if(isEmpty()) {
-//                 return -1;
-//             }
-//             int top = head.data;
-//             head = head.next;
-//             return top;
-//         }
+        public static int pop() {
+            if(isEmpty()) {
+                return -1;
+            }
+            int top = head.data;
+            head = head.next;
+            return top;
+        }
 
-//         public static int peek() {
-//             if(isEmpty()) {
-//                 return -1;
-//             }
-//             return head.data;
-//         }
-//     }
+        public static int peek() {
+            if(isEmpty()) {
+                return -1;
+            }
+            return head.data;
+        }
+    }
 
-//     public static void main (String args[]) {
-//         Stack s = new Stack();
-//         s.push(1);
-//         s.push(2);
-//         s.push(3);
-//         s.push(4);
-//         s.push(5);
+    public static void main (String args[]) {
+        Stack s = new Stack();
+        s.push(1);
+        s.push(2);
+        s.push(3);
+        s.push(4);
+        s.push(5);
 
-//         while(!s.isEmpty()) {
-//             System.out.println(s.peek());
-//             s.pop();
-//         }
-//     }
-// }
+        while(!s.isEmpty()) {
+            System.out.println(s.peek());
+            s.pop();
+        }
+    }
+}
     
 // //===================================================================================================
 // //===================================================================================================
@@ -3349,36 +3349,45 @@ public class second{
 // //===================================================================================================
 // //===================================================================================================
 
-import java.util.*;
+// import java.util.*;
 
-public class stackclass {
-    public static void pushAtBottom(int data, Stack<Integer> s) {
-        if(s.isEmpty()) {
-            s.push(data);
-            return;
-        }
+// public class stackClass {
+//     public static void pushAtBottom(int data, Stack<Integer> s) {
+//         if(s.isEmpty()) {
+//             s.push(data);
+//             return;
+//         }
         
-        int top = s.pop();
-        pushAtBottom(data, s);
-        s.push(top);
-    }
+//         int top = s.pop();
+//         pushAtBottom(data, s);
+//         s.push(top);
+//     }
+
+//     public  static void reverse(Stack<Integer> s) {
+//          if(s.isEmpty()) {
+//             return;
+//          }
+//          int top = s.pop();
+//          reverse(s);
+//          pushAtBottom(top, s);
+//     }
     
-    public static void main (String args[]) {
-        Stack<Integer> s = new Stack<>();
-        s.push(1);
-        s.push(2);
-        s.push(3);
+//     public static void main (String args[]) {
+//         Stack<Integer> s = new Stack<>();
+//         s.push(1);
+//         s.push(2);
+//         s.push(3);
         
-        pushAtBottom(data, s)
+//         reverse(s);
+//         // pushAtBottom(4, s);
 
 
-        while(!s.isEmpty()) {
-            System.out.println(s.peek());
-            s.pop();
-        }
-    }
-
-}
+//         while(!s.isEmpty()) {
+//             System.out.println(s.peek());
+//             s.pop();
+//         }
+//     }
+// }
 // //===================================================================================================
 // //===================================================================================================
 
