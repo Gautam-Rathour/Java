821a37772fc2d44a0c2129be4590045f70c509c5
Gautam-Rathour <gau321tam@gmail.com>
2024-09-30 15:22:16 +0530

    Add

--- stackclass.java
+++ stackclass.java
@@ -3514,24 +3514,26 @@ public class second{
 // //===============================================================================================
 // //==================================================================================================
 
-// Stack :-  ( By collection framwork --)
-import java.util.*;
+// // Stack :-  ( By collection framwork --)
+// import java.util.*;
 
-public class StackClass {
-    public static void main(String args[]) {
-        Stack<Integer> s = new Stack<>();
-        s.push(1);
-        s.push(2);
-        s.push(3);
-        s.push(4);
+// public class StackClass {
+//     public static void main(String args[]) {
+//         Stack<Integer> s = new Stack<>();
+//         s.push(1);
+//         s.push(2);
+//         s.push(3);
+//         s.push(4);
 
-        while(!s.isEmpty()) {
-            System.out.println(s.peek());
-            s.pop();
-        }
-    }
-}
+//         while(!s.isEmpty()) {
+//             System.out.println(s.peek());
+//             s.pop();
+//         }
+//     }
+// }
 
+// //===============================================================================================
+// //==================================================================================================
 
 
 
