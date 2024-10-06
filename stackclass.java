b5cd7c97fa091844c64ab19843878d4664d4c01c
Gautam-Rathour <gau321tam@gmail.com>
2024-10-06 14:01:54 +0530

    Add some New Code

--- stackclass.java
+++ stackclass.java
@@ -3570,6 +3570,46 @@ public class second{
 
 // Stack :-  ( Reverse a Stack ----)
 
+import java.util.*;
+public class StackClass {
+    public static void pushAtBottom(int data, Stack<Integer> s) {
+        if(s.isEmpty()) {
+            s.push(data);
+            return;
+        }
+        int top = s.pop();
+        pushAtBottom(data, s);
+        s.push(top);
+
+    }
+
+    public static void reverse(Stack<Integer> s) {
+        if(s.isEmpty()) {
+            return;
+        }
+
+        int top = s.pop();
+        reverse(s);
+        pushAtBottom(top, s);
+    }
+
+    public static void main (String args[]) {
+        Stack<Integer> s = new Stack<>();
+        s.push(1);
+        s.push(2);
+        s.push(3);
+        s.push(4);
+        s.push(5);
+
+        reverse(s);
+        pushAtBottom(6,s);
+
+        while(!s.isEmpty()) {
+            System.out.println(s.peek());
+            s.pop();
+        }
+    }
+}
 
 
 
