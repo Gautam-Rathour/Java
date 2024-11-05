b2b3e98820b46dc24f274f05e0b0d0f0d0ec199c
Gautam-Rathour <gau321tam@gmail.com>
2024-11-05 14:16:38 +0530

    Add

--- Revise.java
+++ Revise.java
@@ -833,19 +833,242 @@ public class Revise {
 // ======================================================================================================
 // ======================================================================================================
 
+// import java.util.*;
+// public class Revise {
+//     public static void main (String args[]) {
+//         Scanner sc = new Scanner (System.in);
+//        int a = 11;
+//        int b = 9;
+//        int c = 6;
+
+//        if (a >= b && a >= c) {
+//         System.out.println("A is greater");
+//        } else if (b >= c) {
+//         System.out.println("B is greater");
+//        } else {
+//         System.out.println("C is greater");
+//        }
+//     }
+// }
+
+// ======================================================================================================
+// ======================================================================================================
+
+// import java.util.*;
+// public class Revise {
+//     public static void main (String args[]) {
+//         Scanner sc = new Scanner (System.in);
+//         int num = 57;
+
+//         String type = (num % 2 == 0) ? "Even" : "Odd";
+//         System.out.println(type);
+
+       
+//     }
+// }
+
+// ======================================================================================================
+// ======================================================================================================
+
+// import java.util.*;
+// public class Revise {
+//     public static void main (String args[]) {
+//         Scanner sc = new Scanner (System.in);
+//         int marks = 31;
+
+//         String result = (marks >= 33) ? "PASS" : "FAIL";
+//         System.out.println(result);
+//     }
+// }
+
+// // ======================================================================================================
+// // ======================================================================================================
+
+// import java.util.*;
+// public class Revise {
+//     public static void main (String args[]) {
+//         Scanner sc = new Scanner (System.in);
+//         int button = sc.nextInt();
+
+        
+//         switch(button) {
+//             case 1: System.out.println("Samosa");
+//             break;
+//             case 2: System.out.println("Burger");
+//             break;
+//             case 3: System.out.println("mango shake");
+//             break;
+//             case 4: System.out.println("We wake up");
+//             break;
+//             default : System.out.println("Nothing");
+//         }
+//     }
+// }
+
+// ======================================================================================================
+// ======================================================================================================
 
 
+// import java.util.*;
+// public class Revise {
+//     public static void main (String args[]) {
+//         Scanner sc = new Scanner (System.in);
+//         int button = sc.nextInt();
+
+        
+//         switch(button) {
+//             case 1: System.out.println("Samosa");
+//             break;
+//             case 2: System.out.println("Burger");
+//             break;
+//             case 3: System.out.println("mango shake");
+//             break;
+//             case 4: System.out.println("We wake up");
+//             break;
+//             default : System.out.println("Nothing");
+//         }
+//     }
+// }
+
+// // ======================================================================================================
+// // ======================================================================================================
 
 
+// import java.util.*;
+// public class Revise {
+//     public static void main (String args[]) {
+//         Scanner sc = new Scanner (System.in);
+ 
+//         System.out.print("Enter first number :- ");
+//         int a = sc.nextInt();
+//         System.out.print("Enter second number :- ");
+//         int b = sc.nextInt();
 
+//         System.out.println("Addtion : 1 ");
+//         System.out.println("Subtract : 2 ");
+//         System.out.println("Multiplaction : 3 ");
+//         System.out.println("Devision : 4 ");
 
+//         System.out.println(" ");
+//             int button = sc.nextInt();
 
+//         switch(button) {
+//             case 1: System.out.println(a + b);
+//             break;
+//             case 2: System.out.println(a - b);
+//             break;
+//             case 3: System.out.println(a * b);
+//             break;
+//             case 4: System.out.println(a / b);
+//             break;
+//             default : System.out.println("Please chick right Button ");
+//         }
+//     }
+// }
 
+// ======================================================================================================
+// ======================================================================================================
 
+// import java.util.*;
+// public class Revise {
+//     public static void main (String args[]) {
+//         Scanner sc = new Scanner (System.in);
+//         int num = sc.nextInt();
 
+//         if(num < 0) {
+//             System.out.println("This number is negative");
+//         } else {
+//             System.out.println("This number is positive");
+//         }
+        
+//     }
+// }
 
+// // ======================================================================================================
+// // ======================================================================================================
 
+// import java.util.*;
+// public class Revise {
+//     public static void main (String args[]) {
+//         Scanner sc = new Scanner (System.in);
+//         double temp = sc.nextInt();
 
+//         if(temp >= 100) {
+//             System.out.println("You have fever");
+//         } else {
+//             System.out.println("You don't have fever");
+//         }
+//     }
+// }
+
+// // ======================================================================================================
+// // ======================================================================================================
+
+// import java.util.*;
+// public class Revise {
+//     public static void main (String args[]) {
+//         Scanner sc = new Scanner (System.in);
+ 
+
+//         System.out.println(" ");
+//             int num = sc.nextInt();
+
+//         switch(num) {
+//             case 1: System.out.println("Sunday");
+//             break;
+//             case 2: System.out.println("Monday");
+//             break;
+//             case 3: System.out.println("Tuesday");
+//             break;
+//             case 4: System.out.println("wednesday");
+//             break;
+//             case 5: System.out.println("Thursday");
+//             break;
+//             case 6: System.out.println("Friday");
+//             break;
+//             case 7: System.out.println("Saturday");
+//             break;
+//             default : System.out.println("Please chick right Button ");
+//         }
+//     }
+// }
+
+// // ======================================================================================================
+// // ======================================================================================================
+
+// import java.util.*;
+// public class Revise {
+//     public static void main (String args[]) {
+//        int a = 63, b = 36;
+//        boolean x = (a < b) ? true : false;
+//        int y = (a > b) ? a : b;   
+
+//        System.out.println(x);
+//        System.out.println(y);
+
+//     }
+// }
+
+// // ======================================================================================================
+// // ======================================================================================================
+
+// import java.util.*;
+// public class Revise {
+//     public static void main (String args[]) {
+//         Scanner sc = new Scanner (System.in);
+//        int year = sc.nextInt();
+
+//        if(year % 4 == 0) {
+//         System.out.println("This year is a leap year");
+//        } else {
+//         System.out.println("This year is not a leap year");
+
+//        }
+//     }
+// }
+
+// // ======================================================================================================
+// // ======================================================================================================
 
 
 
