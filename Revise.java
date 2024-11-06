f92e94bcee5bcffcd4aeb3a97995a78434b75596
Gautam-Rathour <gau321tam@gmail.com>
2024-11-06 23:33:26 +0530

    Add

--- Revise.java
+++ Revise.java
@@ -1093,22 +1093,22 @@ public class Revise {
 // // ======================================================================================================
 // // ======================================================================================================
 
-// Loops :- 
+// // Loops :- 
 
-import java.util.*;
-public class Revise {
-    public static void main (String args[]) {
-        Scanner sc = new Scanner(System.in);
-        int num = sc.nextInt();
+// import java.util.*;
+// public class Revise {
+//     public static void main (String args[]) {
+//         Scanner sc = new Scanner(System.in);
+//         int num = sc.nextInt();
         
-        for(int i=1; i<=num; i++) {
-            for(int j=1; j<=num; j++) {
-                System.out.print("* ");
-            }
-            System.out.println();
-        }
-    }
-}
+//         for(int i=1; i<=num; i++) {
+//             for(int j=1; j<=num; j++) {
+//                 System.out.print("* ");
+//             }
+//             System.out.println();
+//         }
+//     }
+// }
 
 // // ======================================================================================================
 // // ======================================================================================================
