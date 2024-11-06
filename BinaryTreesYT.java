2bda74d4b3793601dc5e47c69690688c0b71959d
Gautam-Rathour <gau321tam@gmail.com>
2024-11-06 23:32:39 +0530

    Add

--- BinaryTreesYT.java
+++ BinaryTreesYT.java
@@ -4177,11 +4177,76 @@ public class second{
 //     }
 // }
 
+// // ======================================================================================================
+// // ======================================================================================================
+
+// // Tree data Structures :-  ( Postorder Traversal );
+
+// public class BinaryTreesYT {
+//     static class Node {
+//         int data;
+//         Node left;
+//         Node right;
+
+//         Node(int data) {
+//             this.data = data;
+//             this.left = null;
+//             this.right = null;
+//         }
+//     }
+
+//     static class BinaryTree {
+//         static int idx = -1;
+//         public static Node buildTree(int nodes[]) {
+//             idx++;
+//             if(nodes[idx] == -1) {
+//                 return null;
+//             }
+
+//             Node newNode = new Node (nodes[idx]);
+
+//             newNode.left = buildTree(nodes);
+//             newNode.right = buildTree(nodes);
+
+//             return newNode;
+//         }
+//     }
+
+//     public static void preorder(Node root) {
+//         if(root == null) {
+//             return; 
+//         }
+//         System.out.print(root.data + " ");
+//         preorder(root.left);
+//         preorder(root.right);
+//     }
+
+//     public static void Postorder(Node root) {
+//         if(root == null) { 
+//             return;
+//         }
+
+//         Postorder(root.left);
+//         Postorder(root.right);
+//         System.out.print(root.data + " ");
+//     }
+
+//     public static void main (String args[]) {
+//         int nodes[] = {1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1};
+//         BinaryTree tree = new BinaryTree();
+//         Node root = tree.buildTree(nodes);
+//         //System.out.println(root.data);
+//         Postorder(root);
+//     }
+// }
+
+
 // ======================================================================================================
 // ======================================================================================================
 
-// Tree data Structures :-  ( Postorder Traversal );
+// Tree data Structures :-  ( Level Order Traversal );
 
+import java.util.*;
 public class BinaryTreesYT {
     static class Node {
         int data;
@@ -4221,27 +4286,72 @@ public class BinaryTreesYT {
         preorder(root.right);
     }
 
-    public static void Postorder(Node root) {
-        if(root == null) { 
+    public static void levelOrder(Node root) {
+        if(root == null) {
             return;
         }
+        Queue<Node> q = new LinkedList<>();
+        q.add(root);
+        q.add(null);
 
-        Postorder(root.left);
-        Postorder(root.right);
-        System.out.print(root.data + " ");
-    }
+        while(!q.isEmpty()) {
+            Node CurrNode = q.remove();
+            if(CurrNode == null) {
+                System.out.println();
+                if(q.isEmpty()) {
+                    break;
+                } else {
+                    q.add(null);
+                }
+            } else {
+                System.out.print(CurrNode.data + " ");
+                if(CurrNode.left != null) {
+                    q.add(CurrNode.left);
+                } 
+
+                if(CurrNode.right != null) {
+                    q.add(CurrNode.right);
+                }
+            }
+        }
+     }
 
     public static void main (String args[]) {
         int nodes[] = {1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1};
         BinaryTree tree = new BinaryTree();
         Node root = tree.buildTree(nodes);
         //System.out.println(root.data);
-        Postorder(root);
+        // Postorder(root);
+        levelOrder(root);
     }
 }
 
 // ======================================================================================================
 // ======================================================================================================
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
--- Revise.java
+++ Revise.java
@@ -1070,9 +1070,45 @@ public class Revise {
 // // ======================================================================================================
 // // ======================================================================================================
 
+// // Loops :- 
 
+// import java.util.*;
+// public class Revise {
+//     public static void main (String args[]) {
+//         Scanner sc = new Scanner(System.in);
+//         int number = sc.nextInt();
+//         int sum = 0;
+        
+//         int n = 1;
+//         while(n <= number) {
+//             // System.out.println(n );
+//             sum += n;
+            
+//             n++;
+//         }
+//         System.out.println(sum);
+//     }
+// }
 
+// // ======================================================================================================
+// // ======================================================================================================
 
+// Loops :- 
+
+import java.util.*;
+public class Revise {
+    public static void main (String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        
+        for(int i=1; i<=num; i++) {
+            for(int j=1; j<=num; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
 
 
 
