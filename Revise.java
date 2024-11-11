6e1b92c60d27f3c57812afd7090d6f13a033d756
Gautam-Rathour <gau321tam@gmail.com>
2024-11-11 13:37:55 +0530

    Add

--- Revise.java
+++ Revise.java
@@ -1673,23 +1673,23 @@ public class Revise {
 // =======================================================================================================
 
 
-public class Revise {
-    public static void main (String args[]) {
-        for(int i=1; i<=5; i++) {
-            for(int j=1; j<=5-i+1; j++) {
-                System.out.print(" ");
-            }
-            for(int j=i; j>=1; j--) {
-                System.out.print(j);
-            }
+// public class Revise {
+//     public static void main (String args[]) {
+//         for(int i=1; i<=5; i++) {
+//             for(int j=1; j<=5-i+1; j++) {
+//                 System.out.print(" ");
+//             }
+//             for(int j=i; j>=1; j--) {
+//                 System.out.print(j);
+//             }
 
-            for(int j=2; j<=i; j++) {
-                System.out.print(j);
-            }
-            System.out.println();
-        }
-    }
-}
+//             for(int j=2; j<=i; j++) {
+//                 System.out.print(j);
+//             }
+//             System.out.println();
+//         }
+//     }
+// }
 
 // =======================================================================================================
 // =======================================================================================================
