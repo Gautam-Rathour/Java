55f0a8e87f7c361f21d6c19f7daca8da9e03bb6b
Gautam-Rathour <gau321tam@gmail.com>
2024-11-19 09:49:47 +0530

    Add

--- tempCodeRunnerFile.java
+++ tempCodeRunnerFile.java
@@ -1,9 +1,9 @@
- for(int j=1; j<=( 2 * i)-1; j++) {
-                System.out.print(number + " ");
-
-                if(j < i) {
-                    number++;
-                } else {
-                    number--;
-                }
-            }
+for(int i=n; i>0; i--) {
+        //     for(int j=0; j<(n-i)-1; j++) {
+        //         System.out.print(" ");
+        //     }
+        //     for(int j=0; j<(2*i)+1; j++) {
+        //         System.out.print("*");
+        //     }
+        //     System.out.println();
+        //  }
