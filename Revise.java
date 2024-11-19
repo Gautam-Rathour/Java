62a82978f4a086b3e02849818fa3e191acac0769
Gautam-Rathour <gau321tam@gmail.com>
2024-11-19 13:58:12 +0530

    Add new code

--- Revise.java
+++ Revise.java
@@ -2471,19 +2471,30 @@ public class Revise {
 // =======================================================================================================
 // =======================================================================================================
 
-// public class Revise {
-//     public static void main (String args[]) {
-//         int n = 5;
+public class Revise {
+    public static void main (String args[]) {
+        int n = 5;
+        
 
-//         for(int i=1; i<=n; i++) {
-//             char ch = (char)('A' + i-1);
-//             for(int j=1; j<=i; j++) {
-//                 System.out.print(ch + " ");
-//             }
-//             System.out.println();
-//         }
-//     }
-// }
+        for(int i=0; i<n; i++) {
+            for(int j=0; j<n-i-1; j++) {
+                System.out.print(" ");
+            }
+            
+            char ch = 'A';
+            for(int j=0; j<(2*i+1); j++) {
+                System.out.print(ch);
+                
+                if(j < i) {
+                    ch++;
+                } else {
+                    ch--;
+                }
+            }
+            System.out.println();
+        }
+    }
+}
 
 
 
