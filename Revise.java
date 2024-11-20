1a491f51d3fda73724bd89c979a3618e9bccc048
Gautam-Rathour <gau321tam@gmail.com>
2024-11-20 07:36:36 +0530

    Add

--- Revise.java
+++ Revise.java
@@ -2468,36 +2468,69 @@ public class Revise {
 // }
 
 
-// =======================================================================================================
-// =======================================================================================================
+// // =======================================================================================================
+// // =======================================================================================================
 
-public class Revise {
-    public static void main (String args[]) {
-        int n = 5;
+// public class Revise {
+//     public static void main (String args[]) {
+//         int n = 5;
         
 
-        for(int i=0; i<n; i++) {
-            for(int j=0; j<n-i-1; j++) {
-                System.out.print(" ");
-            }
+//         for(int i=0; i<n; i++) {
+//             for(int j=0; j<n-i-1; j++) {
+//                 System.out.print(" ");
+//             }
             
-            char ch = 'A';
-            for(int j=0; j<(2*i+1); j++) {
-                System.out.print(ch);
+//             char ch = 'A';
+//             for(int j=0; j<(2*i+1); j++) {
+//                 System.out.print(ch);
                 
-                if(j < i) {
-                    ch++;
-                } else {
-                    ch--;
+//                 if(j < i) {
+//                     ch++;
+//                 } else {
+//                     ch--;
+//                 }
+//             }
+//             System.out.println();
+//         }
+//     }
+// }
+
+
+// // =======================================================================================================
+// // =======================================================================================================
+
+import java.util.*;
+public class Revise {
+    public static void main(String args[]) {
+        int arr[] = {1, -2, 6, -1, 3};
+        int n = arr.length;
+        int currSum = 0;
+        int add = 0;
+        int maxSum = Integer.MIN_VALUE;
+        int prefix[] = new int[arr.length];
+
+        prefix[0] = arr[0];
+        //calculate prefix array :-
+        for(int i=1; i<prefix.length; i++) {
+            prefix[i] = prefix[i-1] + arr[i];
+        }
+
+        for(int i=0; i<n; i++) {
+            int start = i;
+            for(int j=i; j<n; j++) {
+                int end = j;
+
+                currSum = start == 0 ? prefix[end] : prefix[end] - prefix[start-1];
+                
+                if(currSum > maxSum) {
+                    maxSum = currSum;
                 }
             }
-            System.out.println();
         }
+        System.out.println("Maximum Sum : " + maxSum);
     }
 }
-
-
-
 
 
 
