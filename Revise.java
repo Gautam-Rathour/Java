c3aff26013c6b1ba945a2bbc5645ee9dee1e0699
Gautam-Rathour <gau321tam@gmail.com>
2024-11-21 21:10:06 +0530

    Add

--- Revise.java
+++ Revise.java
@@ -2553,10 +2553,20 @@ public class Revise {
 // =======================================================================================================
 // =======================================================================================================
 
+// Linear Search :-
 import java.util.*;
 public class Revise {
     public static void main(String args[]) {
-      
+        int arr[] = {1,2,3,4,5,6,7,11};
+        int n = arr.length;
+        int key = 6;
+
+        for(int i=0; i<n; i++) {
+            
+            if(arr[i] == key) {
+                System.out.println("Element is present at index = " + i);
+            }
+        }
     }
 }
 
