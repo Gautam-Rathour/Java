62fb850183401d9011268753faf458ae607cf645
Gautam-Rathour <gau321tam@gmail.com>
2024-11-24 16:19:16 +0530

    Add

--- Matrices.java
+++ Matrices.java
@@ -71,10 +71,105 @@
 // =======================================================================================================
 // =======================================================================================================
 
+// // Sprial matrix Code :-
+
+// import java.util.*;
+// public class Matrices {
+//     public static void printSpiral (int matrix[][]) {
+//         int startRow = 0;
+//         int startCol = 0;
+//         int endRow = matrix.length - 1;
+//         int endCol = matrix[0].length-1;
+
+//         while(startRow <= endRow && startCol <= endCol) {
+//             //top
+//             for(int j=startCol; j<=endCol; j++) {
+//                 System.out.print(matrix[startRow][j] + " ");
+//             }
+
+//             //right
+//             for(int i=startRow+1; i<=endRow; i++) {
+//                 System.out.print(matrix[i][endCol]+ " ");
+//             }
+
+//             //bottom
+//             for(int j=endCol-1; j>=startCol; j--) {
+//                 if(startRow == endRow) {
+//                     break;
+//                 }
+//                 System.out.print(matrix[endRow][j] +" ");
+//             }
+
+//             //left
+//             for(int i=endRow-1; i>=startRow+1; i--) {
+//                 if(startCol == endCol) {
+//                     break;
+//                 }
+//                 System.out.print(matrix[i][startCol] + " ");
+//             }
+
+//             startCol++;
+//             startRow++;
+//             endCol--;
+//             endRow--;
+//         }
+//         System.out.println();
+//     }
+//     public static void main(String args[]) {
+//         int matrix[][] = {{1, 2, 3, 4},
+//                           {5, 6, 7, 8},
+//                           {9, 10, 11, 12},
+//                           {13, 14, 15, 16}};
+
+//                           printSpiral(matrix);
+//     }
+// }
+
+// =======================================================================================================
+// =======================================================================================================
+
+//Diagonal Sum :-
+//Given a square matrix, find the sum of the main diagonal (from top-left to bottom-right and top-right to bottom-left;
+
+import java.util.*;
+public class Matrices {
+    public static int diagonalSum(int matrix[][]) {
+        int sum = 0;
+
+        for(int i=0; i<matrix.length; i++) {
+            for(int j=0; j<matrix[0].length; j++) {
+                if(i == j) {
+                    sum += matrix[i][j];
+                }
+
+                if(i+j == matrix.length-1) {
+                    sum += matrix[i][j];
+                }
+            }
+        }
+
+        // for(int i=0; i<matrix.length; i++) {
+        //     //pd
+        //     sum += matrix[i][i];
+        //     //sd
+        //     if(i != matrix.length-i-1)
+        //     sum += matrix[i][matrix.length-i-1];
+        // }
+        return sum;
+    }
+    public static void main (String args[]) {
+        int matrix[][] = {{1, 2, 3, 4},
+                          {5, 6, 7, 8},
+                          {9, 10, 11, 12},
+                          {13, 14, 15, 16}};
+
+                          System.out.println(diagonalSum(matrix));
+    }
+}
 
 
-
-
+// =======================================================================================================
+// =======================================================================================================
 
 
 
