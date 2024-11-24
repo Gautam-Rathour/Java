87ceada477912d0b064ab9711b019c46d1caba28
Gautam-Rathour <gau321tam@gmail.com>
2024-11-24 20:23:29 +0530

    Add

--- Matrices.java
+++ Matrices.java
@@ -128,48 +128,87 @@
 // =======================================================================================================
 // =======================================================================================================
 
-//Diagonal Sum :-
-//Given a square matrix, find the sum of the main diagonal (from top-left to bottom-right and top-right to bottom-left;
+// //Diagonal Sum :-
+// //Given a square matrix, find the sum of the main diagonal (from top-left to bottom-right and top-right to bottom-left;
+
+// import java.util.*;
+// public class Matrices {
+//     public static int diagonalSum(int matrix[][]) {
+//         int sum = 0;
+
+//         for(int i=0; i<matrix.length; i++) {
+//             for(int j=0; j<matrix[0].length; j++) {
+//                 if(i == j) {
+//                     sum += matrix[i][j];
+//                 }
+
+//                 if(i+j == matrix.length-1) {
+//                     sum += matrix[i][j];
+//                 }
+//             }
+//         }
+
+//         // for(int i=0; i<matrix.length; i++) {
+//         //     //pd
+//         //     sum += matrix[i][i];
+//         //     //sd
+//         //     if(i != matrix.length-i-1)
+//         //     sum += matrix[i][matrix.length-i-1];
+//         // }
+//         return sum;
+//     }
+//     public static void main (String args[]) {
+//         int matrix[][] = {{1, 2, 3, 4},
+//                           {5, 6, 7, 8},
+//                           {9, 10, 11, 12},
+//                           {13, 14, 15, 16}};
+
+//                           System.out.println(diagonalSum(matrix));
+//     }
+// }
+
+// =======================================================================================================
+// =======================================================================================================
+
+// Search in sorted matrix :-
 
 import java.util.*;
 public class Matrices {
-    public static int diagonalSum(int matrix[][]) {
-        int sum = 0;
+    public static boolean staircaseSearch(int matrix[][], int key) {
+        int row = 0, col = matrix[0].length-1;
 
-        for(int i=0; i<matrix.length; i++) {
-            for(int j=0; j<matrix[0].length; j++) {
-                if(i == j) {
-                    sum += matrix[i][j];
-                }
+        while(row < matrix.length && col >= 0) {
+            if(matrix[row][col] == key) {
+                System.out.println("Found key at (" + row + "," + col + ")");
+                return true;
+            }
 
-                if(i+j == matrix.length-1) {
-                    sum += matrix[i][j];
-                }
+            else if(key < matrix[row][col]) {
+                col--;
+            }
+            else {
+                row++;
             }
         }
-
-        // for(int i=0; i<matrix.length; i++) {
-        //     //pd
-        //     sum += matrix[i][i];
-        //     //sd
-        //     if(i != matrix.length-i-1)
-        //     sum += matrix[i][matrix.length-i-1];
-        // }
-        return sum;
+        System.out.println("Key not found!");
+        return false;
     }
-    public static void main (String args[]) {
-        int matrix[][] = {{1, 2, 3, 4},
-                          {5, 6, 7, 8},
-                          {9, 10, 11, 12},
-                          {13, 14, 15, 16}};
+    public static void main(String args[]) {
+        int matrix[][] = {{10, 20, 30, 40},
+                          {15, 25, 35, 45},
+                          {27, 29, 37, 48},
+                          {32, 33, 39, 50}};
 
-                          System.out.println(diagonalSum(matrix));
+
+
+        int key = 350;
+        staircaseSearch(matrix, key);
     }
 }
 
+// =======================================================================================================
+// =======================================================================================================
 
-// =======================================================================================================
-// =======================================================================================================
 
 
 
