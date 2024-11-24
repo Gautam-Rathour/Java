2c3e3b8771e3cc9170d5d9283560185f1fefc1d8
Gautam-Rathour <gau321tam@gmail.com>
2024-11-24 08:09:43 +0530

    Add

---
+++ Matrices.java
@@ -0,0 +1,106 @@
+
+// Matricees :- 
+
+import java.util.*;
+public class Matrices {
+    public static boolean search(int matrix[][], int key) {
+        for(int i=0; i<matrix.length; i++) { 
+            for(int j=0; j<matrix[0].length; j++) {
+                if(matrix[i][j] == key) {
+                    System.out.println("found at cell (" + i + "," + j + ")");
+                    return true;
+                }
+            }
+        }
+        System.out.println("The key is not Found!");
+        return false;
+    }
+    // Find largest :-
+    public static void searchMax(int matrix[][]) {
+        int largest = Integer.MIN_VALUE;
+        for(int i=0; i<matrix.length; i++) {
+            for(int j=0; j<matrix[0].length; j++) {
+                if(matrix[i][j] > largest) {
+                    largest = matrix[i][j];
+                }
+            }
+        }
+        System.out.println("The largest element is : " + largest);
+    }
+    // Find smallest :-
+    public static void searchMin(int matrix[][]) {
+        int smallest = Integer.MAX_VALUE;
+        for(int i=0; i<matrix.length; i++) {
+            for(int j=0; j<matrix[0].length; j++) {
+                if(matrix[i][j] < smallest) {
+                    smallest = matrix[i][j];
+                }
+            }
+        }
+        System.out.println("The smallest element is : " + smallest);
+    }
+
+    public static void main (String args[]) {
+        Scanner scanner = new Scanner(System.in);
+        int matrix[][] = new int[3][3];
+        int n = 3, m = 3;
+        int key = 7;
+
+        System.out.print("Enter the number : ");
+        for(int i=0; i<n; i++) {
+            for(int j=0; j<m; j++) {
+                matrix[i][j] = scanner.nextInt();
+            }
+        }
+        //Output
+        for(int i=0; i<n; i++) { 
+            for(int j=0; j<m; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+        search(matrix, key);
+
+
+        searchMax(matrix);
+        searchMin(matrix);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
