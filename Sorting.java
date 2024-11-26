bdb48da290491bf40cc08f473a635c9934f5c029
Gautam-Rathour <gau321tam@gmail.com>
2024-11-26 08:27:54 +0530

    Add

--- Sorting.java
+++ Sorting.java
@@ -217,35 +217,35 @@
 // =======================================================================================================
 // =======================================================================================================
 
-// Live class :-
-public class Sorting {
-    public static void selectionSort(int arr[], int n) {
-        
-        for(int i=0; i<n-1; i++) {
-            int maxIndex = i;
+// // Live class :-
+// public class Sorting {
+//     public static void selectionSort(int arr[]) {
+//         int n = arr.length;
 
-            for(int j=i+1; j<n; j++) {
-                if(arr[j] > arr[maxIndex]) {
-                    maxIndex = j;
-                }
-            }
-            int temp = arr[i];
-            arr[i] = arr[maxIndex];
-            arr[maxIndex] = temp;
+//         for(int i=0; i<n; i++) {
+//             int manIndex = i;
+//             for(int j=i+1; j<n; j++) {
+//                 if(arr[j] > arr[manIndex]) {
+//                     manIndex = j;
+//                 }
+//             }
+//             // Swap
+//             int temp = arr[i];
+//             arr[i] = arr[manIndex];
+//             arr[manIndex] = temp;
+//         }
+//     }
+//     public static void main (String args[]) {
+//         int arr[] = {3, 6, 2, 1, 8};
 
-        }
-    }
-    public static void main(String[] args) {
-        int arr[] = {1, 3, 5, 7, 9};
-        int n = arr.length;
 
-        selectionSort(arr, n);
+//         selectionSort(arr);
 
-        for(int i=0; i<n; i++) {
-            System.out.print(arr[i] + " ");
-        }
-    }
-}
+//         for(int i=0; i<arr.length; i++) {
+//             System.out.print(arr[i] + " ");
+//         }
+//     }
+// }
 
 
 
