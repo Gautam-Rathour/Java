bfb6bfb776dc4fab6b3b16c6178fe91983924839
Gautam-Rathour <gau321tam@gmail.com>
2024-12-03 11:30:24 +0530

    Add

--- OOPS.java
+++ OOPS.java
@@ -98,8 +98,19 @@
 public class OOPS {
     public static void main(String args[]) {
         Student s1 = new Student();
-       
+        s1.name = "shahu";
+        s1.roll = 123;
+        s1.password = "abcd";
+        s1.marks[0] = 100;
+        s1.marks[1] = 90;
+        s1.marks[2] = 80;
+        
+       Student s2 = new Student(s1);
+        s2.password = "xyz";
 
+        for(int i=0; i<3; i++) {
+            System.out.println(s2.marks[i]);
+        }
       
     }
 }
@@ -107,13 +118,32 @@ public class OOPS {
 class Student {
     String name;
     int roll;
+    String password;
+    int marks[];
 
-    Student() {
-        // this.name = name;
-        System.out.println("Constructor is colled ....");
+
+    //copy constructor
+    Student (Student s1) {
+        marks = new int[3];
+        this.name = s1.name;
+        this.roll = s1.roll;
+        this.marks = s1.marks;
+    }
+
+
+  
+    Student(String name) {
+        marks = new int[3];
+        return this.name;
+    }
+    Student(int roll) {
+        marks = new int[3];
+        return this.roll;
     }
 }
 
+// ===========================================================================================================
+// ===========================================================================================================
 
 
 
