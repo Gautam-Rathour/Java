8482c9495680eceab4ef9b5eb3d3a9e8f3e9327a
Gautam-Rathour <gau321tam@gmail.com>
2024-12-05 22:12:47 +0530

    Add

--- Recursion.java
+++ Recursion.java
@@ -113,28 +113,29 @@
 // =======================================================================================================
 // =======================================================================================================
 
+// // Find fibonacci by  Recursion :- 
 
-// Find fibonacci by  Recursion :- 
+// public class Recursion {
+//     public static boolean isSorted(int arr[], int i) {
+//         if(i == arr.length-1) {
+//             return true;
+//         }
+//         if(arr[i] > arr[i + 1]) {
+//             return false;
+//         }
 
-public class Recursion {
-    public static boolean isSorted(int arr[], int i) {
-        if(i == arr.length-1) {
-            return true;
-        }
-        if(arr[i] > arr[i + 1]) {
-            return false;
-        }
+//         return isSorted(arr, i+1);
+//     }
+//     public static void main(String args[]) {
+//         int arr[] = {1, 2, 3, 5, 4};
 
-        return isSorted(arr, i+1);
-    }
-    public static void main(String args[]) {
-        int arr[] = {1, 2, 3, 5, 4};
-
-        System.out.println(isSorted(arr, 0));
+//         System.out.println(isSorted(arr, 0));
         
-    }
-}
+//     }
+// }
 
+// =======================================================================================================
+// =======================================================================================================
 
 
 
