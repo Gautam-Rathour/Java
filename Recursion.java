4f06987b1756852c52c67570816db1d495d69877
Gautam-Rathour <gau321tam@gmail.com>
2024-12-07 13:00:38 +0530

    Add

--- Recursion.java
+++ Recursion.java
@@ -341,7 +341,7 @@
 // =======================================================================================================
 // =======================================================================================================
 
-// Second Lecture :-
+// // Second Lecture :-
 
 // public class Recursion {
 //     public static int tilingProblem(int n) {
@@ -350,38 +350,85 @@
 //             return 1;
 //         }
 
-//         // kaam
-//         // vertical choice
-//         int fnm1 = tilingProblem(n - 1);
+//         // // kaam
+//         // // vertical choice
+//         // int fnm1 = tilingProblem(n - 1);
 
-//         // horizontal choice 
-//         int fnm2 = tilingProblem(n - 2);
+//         // // horizontal choice 
+//         // int fnm2 = tilingProblem(n - 2);
+//         // int totWays = fnm1 + fnm2;
+//         // return totWays;
 
-//         int totWays = fnm1 + fnm2;
-
-//         return totWays;
+//         return tilingProblem(n-1) + tilingProblem(n-2);
 //     }
 //     public static void main(String args[]) {
 //         System.out.println(tilingProblem(4));
 //     }
 // }
 
-// =======================================================================================================
-// =======================================================================================================
+// // =======================================================================================================
+// // =======================================================================================================
+
+// // Remove Duplicates letter :-
 
 // public class Recursion {
-  
-//     public static void main(String args[]) {
+//     public static void removeDuplicates(String str, int idx, StringBuilder newStr, boolean map[]) {
+//         if(idx == str.length()) {
+//             System.out.print(newStr);
+//             return;
+//         }
+//             //kaam
+//             char currChar = str.charAt(idx);
+//             if(map[currChar-'a'] == true) {
+//                 //duplicate
+//                 removeDuplicates(str, idx+1, newStr, map);
+//             } else {
+//                 map[currChar-'a'] = true;
+//                 removeDuplicates(str, idx+1, newStr.append(currChar),map);
+//             }
+//         }
         
+//     public static void main(String args[]) {
+//         String str = "appnnacollege";
 
+//         removeDuplicates(str, 0, new StringBuilder(""), new boolean[26]);
+//         System.out.println();
 //     }
 // }
 
 // =======================================================================================================
 // =======================================================================================================
 
+// Find Friends Pairing :- 
 
+public class Recursion {
+    public static int friendsPairing(int n) {
+        if(n == 1 || n == 2) {
+            return n;
+        }
+        
+        // // choice
+        // //single
+        // int fnm1 = friendsPairing(n-1);
 
+        // //pair
+        // int fnm2 = friendsPairing(n-2);
+        // int pairWays = (n-1) * fnm2;
+
+        // // totWays
+        // int totWays = fnm1 + pairWays;
+        // return totWays;
+
+        return friendsPairing(n-1) + (n-1) * friendsPairing(n-2);
+    }
+    public static void main(String args[]) {
+        System.out.println(friendsPairing(3));
+
+    }
+}
+
+// =======================================================================================================
+// =======================================================================================================
 
 
 
