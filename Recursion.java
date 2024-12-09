ff0012dfa9126ea18f3f7014ad7a4d0d5a7204f0
Gautam-Rathour <gau321tam@gmail.com>
2024-12-09 10:25:22 +0530

    Add

--- Recursion.java
+++ Recursion.java
@@ -463,28 +463,28 @@
 // =======================================================================================================
 // =======================================================================================================
 
-//   Tower of hanoi :--
+// //   Tower of hanoi :--
 
-public class Recursion {
-    public static void toh(int n, int A, int C, int B) {
-        if(n == 0) {
-            return;
-        }
-        toh(n-1, A, B, C);
-        System.out.println("Moving " + n + "th disk from " + A + " to " + C);
-        toh(n-1, B, C, A);
-    }
-    public static void main(String args[]) {
-        toh(3, 10, 30, 20);
-    }
-}
+// public class Recursion {
+//     public static void toh(int n, int A, int C, int B) {
+//         if(n == 0) {
+//             return;
+//         }
+//         toh(n-1, A, B, C);
+//         System.out.println("Moving " + n + "th disk from " + A + " to " + C);
+//         toh(n-1, B, C, A);
+//     }
+//     public static void main(String args[]) {
+//         toh(3, 10, 30, 20);
+//     }
+// }
 
 // =======================================================================================================
 // =======================================================================================================
 
 public class Recursion {
     public static void main (String args[]) {
-        
+        System.out.println("hello world");
     }
 }
 
