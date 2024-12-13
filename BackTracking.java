2f466a9d5abb6197164ee01e5103df89e67d2e7a
Gautam-Rathour <gau321tam@gmail.com>
2024-12-13 21:25:34 +0530

    Add

--- BackTracking.java
+++ BackTracking.java
@@ -32,35 +32,60 @@
 // =======================================================================================================
 // =======================================================================================================
 
+// // Find Sub-sets :-
+
+// public class BackTracking {
+//     public static void findSubsets(String str, String ans, int i) {
+//         // base case
+//         if(i == str.length()) {
+//             if(ans.length() == 0) {
+//                 System.out.println("null");
+//             } else { 
+//                 System.out.println(ans);
+//             }
+//             return;
+//         }
+
+//         //recursion 
+//         //Yes choice
+//         findSubsets(str, ans+str.charAt(i), i+1);
+//         //No choice
+//         findSubsets(str, ans, i+1);
+
+//     }
+//     public static void main(String args[]) {
+//         String str = "abc";
+//         findSubsets(str, "", 0);
+        
+//     }
+    
+// }
+
+// =======================================================================================================
+// =======================================================================================================
+
+// Find permutation : --
+
 public class BackTracking {
-    public static void findSubsets(String str, String ans, int i) {
+    public static void findPermutation(String str, String ans) {
         // base case
-        if(i == str.length()) {
-            if(ans.length() == 0) {
-                System.out.println("null");
-            } else { 
-                System.out.println(ans);
-            }
+        if(str.length() == 0) {
+            System.out.println(ans);
             return;
         }
-
-        //recursion 
-        //Yes choice
-        findSubsets(str, ans+str.charAt(i), i+1);
-        //No choice
-        findSubsets(str, ans, i+1);
-
+        //recursion
+        for(int i=0; i<str.length(); i++) {
+            char curr = str.charAt(i);
+            //"abcde" => "ab" + "de" = "abde"
+            String NewStr = str.substring(0, i) + str.substring(i+1);
+            findPermutation(NewStr, ans + curr);
+        }
     }
     public static void main(String args[]) {
         String str = "abc";
-        findSubsets(str, "", 0);
-        
+        findPermutation("abc", "");
     }
-    
 }
-
-
-
 
 
 
