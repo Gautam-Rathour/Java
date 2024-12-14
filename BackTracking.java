0c740e87c9444cfc32338bfeb982071952d4d14b
Gautam-Rathour <gau321tam@gmail.com>
2024-12-14 13:03:37 +0530

    add

--- BackTracking.java
+++ BackTracking.java
@@ -90,6 +90,77 @@
 // =======================================================================================================
 // =======================================================================================================
 
+// public class BackTracking {
+//     public static boolean isSafe(char board[][], int row, int col) {
+//         // vertical up
+//         for(int i=row-1; i>=0; i--) {
+//             if(board[i][col] == 'Q') {
+//                 return false;
+//             }
+//         }
+
+//         // diag left up 
+//         for(int i=row-1, j=col-1; i>=0 && j>=0; i-- , j--) {
+//             if(board[i][j] == 'Q') {
+//                 return false;
+//             }
+//         }
+
+//         // diag right up
+//         for(int i=row-1, j=col+1; i>=0 && j<board.length; i--, j++) {
+//             if(board[i][j] == 'Q') {
+//                 return false;
+//             }
+//         }
+//         return true;
+//     }
+
+
+//     static int count = 0;
+//     public static void nQueens(char board[][], int row) {
+//         //base
+//         if(row == board.length) {
+//             // printBoard(board);
+//             count++;
+//             return;
+//         }
+//         // column loop
+//         for(int j=0; j<board.length; j++) {
+//             if(isSafe(board, row, j)) {
+//                 board[row][j] = 'Q';
+//                 nQueens(board, row+1); // function call
+//                 board[row][j] = 'x';   // backtracking step
+//             }
+//         }
+//     }
+//     public static void printBoard(char board[][]) {
+//         System.out.println("------------ chess board ------------");
+//         for(int i=0; i<board.length; i++) {
+//             for(int j=0; j<board.length; j++) {
+//                 System.out.print(board[i][j] + " ");
+//             }
+//             System.out.println();
+//         }
+//     }
+
+//     public static void main(String args[]) {
+//         int n = 5;
+//         char board[][] = new char[n][n];
+//         //initialize
+//         for(int i=0; i<n; i++) {
+//             for(int j=0; j<n; j++) {
+//                 board[i][j] = 'x';
+//             }
+//         }
+
+//         nQueens(board, 0);
+//         System.out.println("Total ways to solve n queens = " + count);
+//     }
+// }
+
+// =======================================================================================================
+// =======================================================================================================
+
 public class BackTracking {
     public static boolean isSafe(char board[][], int row, int col) {
         // vertical up
@@ -114,20 +185,27 @@ public class BackTracking {
         }
         return true;
     }
-    public static void nQueens(char board[][], int row) {
+
+
+    static int count = 0;
+    public static boolean nQueens(char board[][], int row) {
         //base
         if(row == board.length) {
-            printBoard(board);
-            return;
+            // printBoard(board);
+            count++;
+            return true;
         }
         // column loop
         for(int j=0; j<board.length; j++) {
             if(isSafe(board, row, j)) {
                 board[row][j] = 'Q';
-                nQueens(board, row+1); // function call
-                board[row][j] = 'x';   // backtracking step
+                if(nQueens(board, row+1)) { // function call
+                    return true;
+                } 
+                board[row][j] = 'x'; // backtracking step
             }
         }
+        return false;
     }
     public static void printBoard(char board[][]) {
         System.out.println("------------ chess board ------------");
@@ -140,7 +218,7 @@ public class BackTracking {
     }
 
     public static void main(String args[]) {
-        int n = 2;
+        int n = 4;
         char board[][] = new char[n][n];
         //initialize
         for(int i=0; i<n; i++) {
@@ -149,11 +227,15 @@ public class BackTracking {
             }
         }
 
-        nQueens(board, 0);
+        if(nQueens(board, 0)) {
+            System.out.println("\nsolution is possible");
+            printBoard(board);
+        } else {
+            System.out.println("solution is not possible");
+        }
+        // System.out.println("Total ways to solve n queens = " + count);
     }
 }
-
-
 
 
 
