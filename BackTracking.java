cabe4ac89eb719bde7b8c4e92b87e9aaa3bbd0c8
Gautam-Rathour <gau321tam@gmail.com>
2024-12-15 00:38:30 +0530

    Add

--- BackTracking.java
+++ BackTracking.java
@@ -90,6 +90,8 @@
 // =======================================================================================================
 // =======================================================================================================
 
+// Find space for " Q "  : --
+
 // public class BackTracking {
 //     public static boolean isSafe(char board[][], int row, int col) {
 //         // vertical up
@@ -114,7 +116,6 @@
 //         }
 //         return true;
 //     }
-
 
 //     static int count = 0;
 //     public static void nQueens(char board[][], int row) {
@@ -161,89 +162,127 @@
 // =======================================================================================================
 // =======================================================================================================
 
+// Find how many way you can put count : - 
+
+// public class BackTracking {
+//     public static boolean isSafe(char board[][], int row, int col) {
+//         // vertical up
+//         for(int i=row-1; i>=0; i--) {
+//             if(board[i][col] == 'Q') {
+//                 return false;
+//             }
+//         }
+
+//         // diag left up 
+//         for(int i=row-1, j=col-1; i>=0 && j>=0; i-- , j--) {
+//             if(board[i][j] == 'Q') {
+//                 return false;
+//             }
+//         }
+
+//         // diag right up
+//         for(int i=row-1, j=col+1; i>=0 && j<board.length; i--, j++) {
+//             if(board[i][j] == 'Q') {
+//                 return false;
+//             }
+//         }
+//         return true;
+//     }
+
+
+//     static int count = 0;
+//     public static boolean nQueens(char board[][], int row) {
+//         //base
+//         if(row == board.length) {
+//             // printBoard(board);
+//             count++;
+//             return true;
+//         }
+//         // column loop
+//         for(int j=0; j<board.length; j++) {
+//             if(isSafe(board, row, j)) {
+//                 board[row][j] = 'Q';
+//                 if(nQueens(board, row+1)) { // function call
+//                     return true;
+//                 } 
+//                 board[row][j] = 'x'; // backtracking step
+//             }
+//         }
+//         return false;
+//     }
+//     public static void printBoard(char board[][]) {
+//         System.out.println("------------ chess board ------------");
+//         for(int i=0; i<board.length; i++) {
+//             for(int j=0; j<board.length; j++) {
+//                 System.out.print(board[i][j] + " ");
+//             }
+//             System.out.println();
+//         }
+//     }
+
+//     public static void main(String args[]) {
+//         int n = 4;
+//         char board[][] = new char[n][n];
+//         //initialize
+//         for(int i=0; i<n; i++) {
+//             for(int j=0; j<n; j++) {
+//                 board[i][j] = 'x';
+//             }
+//         }
+
+//         if(nQueens(board, 0)) {
+//             System.out.println("\nsolution is possible");
+//             printBoard(board);
+//         } else {
+//             System.out.println("solution is not possible");
+//         }
+//         // System.out.println("Total ways to solve n queens = " + count);
+//     }
+// }
+
+// =======================================================================================================
+// =======================================================================================================
+
+// // Find Frid Ways ( how many way we have for rich the n-1, m-1 ) : --
+
+// public class BackTracking {
+//     public static int gridWays(int i, int j, int n, int m) {  
+//         // base case 
+//         if(i == n-1 && j == m-1) {   // condn for last cell
+//             return 1;
+//         } else if(i == n || j == n) {   // boundary cross condn
+//             return 0;
+//         }
+
+//         int w1 = gridWays(i+1, j, n, m);
+//         int w2 = gridWays(i, j+1, n, m);
+//         return w1 + w2;
+//     }
+//     public static void main(String args[]) {
+//         int n = 3, m = 3;
+//         System.out.println(gridWays(0, 0, n, m));
+//     }
+// }
+
+// =======================================================================================================
+// =======================================================================================================
+
+// Wrinte a Function to complete a Sudoku.
+
 public class BackTracking {
-    public static boolean isSafe(char board[][], int row, int col) {
-        // vertical up
-        for(int i=row-1; i>=0; i--) {
-            if(board[i][col] == 'Q') {
-                return false;
-            }
-        }
-
-        // diag left up 
-        for(int i=row-1, j=col-1; i>=0 && j>=0; i-- , j--) {
-            if(board[i][j] == 'Q') {
-                return false;
-            }
-        }
-
-        // diag right up
-        for(int i=row-1, j=col+1; i>=0 && j<board.length; i--, j++) {
-            if(board[i][j] == 'Q') {
-                return false;
-            }
-        }
-        return true;
-    }
-
-
-    static int count = 0;
-    public static boolean nQueens(char board[][], int row) {
-        //base
-        if(row == board.length) {
-            // printBoard(board);
-            count++;
-            return true;
-        }
-        // column loop
-        for(int j=0; j<board.length; j++) {
-            if(isSafe(board, row, j)) {
-                board[row][j] = 'Q';
-                if(nQueens(board, row+1)) { // function call
-                    return true;
-                } 
-                board[row][j] = 'x'; // backtracking step
-            }
-        }
-        return false;
-    }
-    public static void printBoard(char board[][]) {
-        System.out.println("------------ chess board ------------");
-        for(int i=0; i<board.length; i++) {
-            for(int j=0; j<board.length; j++) {
-                System.out.print(board[i][j] + " ");
-            }
-            System.out.println();
-        }
-    }
 
     public static void main(String args[]) {
-        int n = 4;
-        char board[][] = new char[n][n];
-        //initialize
-        for(int i=0; i<n; i++) {
-            for(int j=0; j<n; j++) {
-                board[i][j] = 'x';
-            }
-        }
-
-        if(nQueens(board, 0)) {
-            System.out.println("\nsolution is possible");
-            printBoard(board);
-        } else {
-            System.out.println("solution is not possible");
-        }
-        // System.out.println("Total ways to solve n queens = " + count);
+        int sudoku[][] = { {0, 0, 8, 0, 0, 0, 0, 0, 0},
+        {4, 9, 0, 1, 5, 7, 0, 0, 2},
+        {0, 0, 3, 0, 0, 4, 1, 9, 0},
+        {1, 8, 5, 0, 6, 0, 0, 2, 0},
+        {0, 0, 0, 0, 2, 0, 0, 6, 0},
+        {9, 6, 0, 4, 0, 5, 3, 0, 0},
+        {0, 3, 0, 0, 7, 2, 0, 0, 4},
+        {0, 4, 9, 0, 3, 0, 0, 5, 7},
+        {8, 2, 7, 0, 0, 9, 0, 1, 3}};
     }
 }
-
-
-
-
-
-
-
-
 
 
 
