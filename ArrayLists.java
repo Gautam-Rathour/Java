d2dadb09e29716b6c8b1d744b1876a158db2d9bb
Gautam-Rathour <gau321tam@gmail.com>
2024-12-18 10:17:43 +0530

    Add

--- ArrayLists.java
+++ ArrayLists.java
@@ -251,48 +251,158 @@
 
 // =======================================================================================================
 // =======================================================================================================
+// // Find stor water with he help of 2 pointer apporach :-
+
+// import java.util.*;
+// public class ArrayLists {
+//     // 2 pointer approach :-
+//     public static int storeWater(ArrayList<Integer> height) {
+//         int maxWater = 0;
+//         int lp = 0;
+//         int rp = height.size()-1;
+
+//         while(lp < rp) {
+//             // calculater water area
+//             int ht = Math.min(height.get(lp), height.get(rp));
+//             int width = rp - lp;
+//             int currWater = ht * width;
+//             maxWater = Math.max(currWater , maxWater);
+
+//             //update ptr
+//             if(height.get(lp) < height.get(rp)) {
+//                 lp++;
+//             } else {
+//                 rp--;
+//             }
+//         }
+//         return maxWater;
+//     }
+
+//     public static void main(String args[]) {
+//         ArrayList<Integer> height = new ArrayList<>();
+//         //  1, 8, 6, 2, 5, 4, 8, 3, 7
+//         height.add(1);
+//         height.add(8);
+//         height.add(6);
+//         height.add(2);
+//         height.add(5);
+//         height.add(4);
+//         height.add(8);
+//         height.add(3);
+//         height.add(7);
+        
+//         System.out.println(storeWater(height));
+//     }
+// }
+
+// =======================================================================================================
+// =======================================================================================================
+
+// // pair Sum - 1  (Find )
+// import java.util.*;
+// public class ArrayLists {
+//     // //Brute Force : - O(n^2)
+//     // public static boolean pairSum1(ArrayList<Integer> list, int target) {
+//     //     for(int i=0; i<list.size(); i++) {
+//     //         for(int j=i+1; j<list.size(); j++) {
+//     //             if(list.get(i) + list.get(j) == target) {
+//     //                 return true;
+//     //             }
+//     //         }
+//     //     }
+//     //     return false;
+//     // }
+
+//     // 2 pointer apporach : -- O(n)
+//     public static boolean pairSum1(ArrayList<Integer> list, int target) {
+//         int lp = 0;
+//         int rp = list.size()-1;
+//          while(lp != rp) {
+//             // case 1
+//             if( list.get(lp) + list.get(rp) == target) {
+//                 return true;
+//             } 
+
+//             // case 2
+//             if(list.get(lp) + list.get(rp) < target) {
+//                 lp++;
+//             }
+            
+//             // case 3
+//             if(list.get(lp) + list.get(rp) > target) {
+//                 rp--;
+//             }
+//         }
+//         return false;
+//     }
+
+//     public static void main(String args[]) {
+//         ArrayList<Integer> list = new ArrayList<>();
+//         list.add(1);
+//         list.add(2);
+//         list.add(3);
+//         list.add(4);
+//         list.add(5);
+//         list.add(6);
+//         int target = 50;
+
+//         System.out.print(pairSum1(list, target));
+
+//     }
+// }
+
+// =======================================================================================================
+// =======================================================================================================
 
 import java.util.*;
 public class ArrayLists {
-    // 2 pointer approach :-
-    public static int storeWater(ArrayList<Integer> height) {
-        int maxWater = 0;
-        int lp = 0;
-        int rp = height.size()-1;
+    public static boolean pairSum1(ArrayList<Integer> list, int target) {
+        int bp = -1;
+        int n = list.size();
 
-        while(lp < rp) {
-            // calculater water area
-            int ht = Math.min(height.get(lp), height.get(rp));
-            int width = rp - lp;
-            int currWater = ht * width;
-            maxWater = Math.max(currWater , maxWater);
-
-            //update ptr
-            if(height.get(lp) < height.get(rp)) {
-                lp++;
-            } else {
-                rp--;
+        for(int i=0; i<list.size(); i++) {
+            if(list.get(i) > list.get(i+1)) { //breaking point
+                bp = i;
+                break;
             }
         }
-        return maxWater;
-    }
+        int lp = bp+1;  // smallest
+        int rp = bp;    // largest
 
+        while (lp != rp) {
+            //case1
+            if(list.get(lp) + list.get(rp) == target) {
+                return true;
+            }
+            //case 2
+            if(list.get(lp) + list.get(rp) < target) {
+                lp = (lp+1) % n;
+            } else {
+                // case 3
+                rp = (n+rp-1) % n;
+            }
+        }
+        return false;
+    }
     public static void main(String args[]) {
-        ArrayList<Integer> height = new ArrayList<>();
-        //  1, 8, 6, 2, 5, 4, 8, 3, 7
-        height.add(1);
-        height.add(8);
-        height.add(6);
-        height.add(2);
-        height.add(5);
-        height.add(4);
-        height.add(8);
-        height.add(3);
-        height.add(7);
-        
-        System.out.println(storeWater(height));
+        ArrayList<Integer> list = new ArrayList<>();
+        // 11, 15, 6, 8, 9, 10 - Sorted & Rotated
+        list.add(11);
+        list.add(15);
+        list.add(6);
+        list.add(8);
+        list.add(9);
+        list.add(10);
+        int target = 16;
+
+        System.out.print(pairSum1(list, target));
     }
 }
+
+
+
+
+
 
 
 
