1cc6096b5042fed9ca9af394bc332c656a04e0de
Gautam-Rathour <gau321tam@gmail.com>
2024-12-19 23:53:43 +0530

    Add

--- Revise.java
+++ Revise.java
@@ -2573,34 +2573,37 @@ public class Revise {
 // =======================================================================================================
 // =======================================================================================================
 
-// Revision :-
-import java.util.*;
-public class Revise {
-    public static void main(String args[]) {
-        HashMap<Integer, Integer> mp = new HashMap<>();
-        mp.put(7, 10);
-        mp.put(3, 5);
-        mp.put(9, 11);
+// // Revision :-
+// import java.util.*;
+// public class Revise {
+//     public static void main(String args[]) {
+//         HashMap<Integer, Integer> mp = new HashMap<>();
+//         mp.put(7, 10);
+//         mp.put(3, 5);
+//         mp.put(9, 11);
 
-        System.out.println("7 " + mp.containsKey(7));
-        System.out.println("10 " + mp.containsKey(10));
-        System.out.println("9 " + mp.containsKey(9));
+//         System.out.println("7 " + mp.containsKey(7));
+//         System.out.println("10 " + mp.containsKey(10));
+//         System.out.println("9 " + mp.containsKey(9));
 
-        // System.out.println(mp.size());
+//         // System.out.println(mp.size());
 
-        // System.out.println(mp.get(7));
-        // System.out.println(mp.get(3));
-        // System.out.println(mp.get(9));
-    }
-}
+//         // System.out.println(mp.get(7));
+//         // System.out.println(mp.get(3));
+//         // System.out.println(mp.get(9));
+//     }
+// }
 
 // =======================================================================================================
 // =======================================================================================================
 
+// // Revision :-
 
 
 
 
+// =======================================================================================================
+// =======================================================================================================
 
 
 
