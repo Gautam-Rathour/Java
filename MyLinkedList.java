2fe33550fe9510749b80097f97a4c8ca8e7c9d5b
Gautam-Rathour <gau321tam@gmail.com>
2024-12-21 13:16:05 +0530

    Add LL Code

--- MyLinkedList.java
+++ MyLinkedList.java
@@ -293,86 +293,290 @@
 
 //     // Detecting Cycle code :----
 
+// import java.util.*;
+//  public class LinkedList {
+//     public static class Node {
+//         int data;
+//         Node next;
+
+//         public Node(int data) {
+//             this.data = data;
+//             this.next = null;
+//         }  
+//     }
+//     public static Node head;
+
+//     public static boolean isCycle() { // Floy's CFA
+//         Node slow = head;
+//         Node fast = head;
+
+//         while(fast != null && fast.next != null) {
+//             fast = fast.next.next; // +2
+//             slow = slow.next;  // +1
+//             if(fast == slow) {
+//                 return true; // isCycle 
+//             }
+//         }
+//         return false;  // cycle doesn't exist
+//     } 
+// // =======================================================================================================
+
+//     // Remove Cycle Code :--
+//     public static void removeCycle() {
+//         // detect cycle
+//         Node slow = head;
+//         Node fast = head;
+//         boolean cycle = false;
+//         while(fast != null && fast.next != null) {
+//             slow = slow.next;
+//             fast = fast.next.next;
+//             if(fast == slow) {
+//                 cycle = true;
+//                 break;
+//             }
+//         }
+//         if(cycle == false) {
+//             return ;
+//         }
+
+//         //find meeting point :--
+//         slow = head;
+//         Node prev = null; // last node
+//         while (slow != fast) {
+//             prev = fast;
+//             slow = slow.next;
+//             fast = fast.next;
+//         }
+
+//         //remove cycle -> last.next = null
+//         prev.next = null;
+//     }
+ 
+//     public static void main(String args[]) {
+//         // LinkedList ll = new LinkedList();
+//         head = new Node(1);
+//         Node temp = new Node(2);
+//         head.next = temp;
+//         head.next.next = new Node(3);
+//         head.next.next.next = temp;
+//         // 1->2->3->2
+//          System.out.println(isCycle());
+//          removeCycle();
+//          System.out.println(isCycle());
+//     }
+//  }
+
+// =======================================================================================================
+// =======================================================================================================
+
+
+// import java.util.*;
+// public class LinkedList {
+//     public static class Node{
+//         int data;
+//         Node next;
+
+//         public Node(int data) {
+//             this.data = data;
+//             this.next = null;
+//         }
+//     }
+//     public static Node head;
+
+//     // check LL is in cycle or not :-
+//     public static boolean isCycle() {
+//         Node slow = head;
+//         Node fast = head;
+
+//         while (fast != null && fast.next != null) {
+//             slow = slow.next;
+//             fast = fast.next.next;
+//             if(fast == slow) {
+//                 return true;
+//             }
+//         }
+//         return false;
+//     }
+
+
+//     // delete the ll cycle :--
+//     public static void removeCycle() {
+//         //check cycle or not --
+//         Node slow = head;
+//         Node fast = head;
+//         boolean cycle = false;
+
+//         while(fast.next != null && fast != null) {
+//             slow = slow.next;
+//             fast = fast.next.next;
+//             if(fast == slow) {
+//                 cycle = true;
+//                 break;
+//             }
+//         }
+//         if(cycle = false) {
+//             return;
+//         }
+        
+
+//         // find the ll cycle connecting point --
+//         slow = head;
+//         Node prev = null;
+//         while(slow != fast) {
+//             prev = fast;
+//             slow = slow.next;
+//             fast = fast.next;
+//         }
+
+//         // delete the cunnection --
+//         prev.next = null;
+
+//     }
+//     public static void main(String args[]) {
+//         head = new Node (1);
+//         Node temp = new Node (2);
+//         head.next = temp;
+//         head.next.next = new Node(3);
+//         head.next.next.next = temp;
+
+//         System.out.println(isCycle());
+//         removeCycle();
+//         System.out.println(isCycle());
+//     }
+// }
+
+// // =======================================================================================================
+// // =======================================================================================================
+
+// import java.util.*;
+// public class MyLinkedList {
+
+//     public static void main(String args[]) {
+//         //create -
+//         LinkedList<Integer> ll = new LinkedList<>();
+
+//         //add 
+//         ll.addLast(1);
+//         ll.addLast(2);
+//         ll.addFirst(0);
+//         //0->1->2
+//         System.out.println(ll);
+//         //remove
+//         ll.removeLast();
+//         ll.removeFirst();
+//         System.out.println(ll);
+//     }
+// }
+
+// =======================================================================================================
+// =======================================================================================================
+
 import java.util.*;
- public class LinkedList {
-    public static class Node {
+public class MyLinkedList {
+    public static class Node{
         int data;
         Node next;
 
-        public Node(int data) {
+        public Node (int data) {
             this.data = data;
             this.next = null;
-        }  
-    }
-    public static Node head;
-
-    public static boolean isCycle() { // Floy's CFA
-        Node slow = head;
-        Node fast = head;
-
-        while(fast != null && fast.next != null) {
-            fast = fast.next.next; // +2
-            slow = slow.next;  // +1
-            if(fast == slow) {
-                return true; // isCycle 
-            }
         }
-        return false;  // cycle doesn't exist
-    } 
-// =======================================================================================================
+    }
 
-    // Remove Cycle Code :--
-    public static void removeCycle() {
-        // detect cycle
+    public static Node head;
+    // add-First :-
+    public static void addFirst(int data) {
+        Node newNode = new Node(data);
+        if(head == null) {
+            head = newNode;
+            return;
+        }
+        newNode.next = head;
+        head = newNode;
+    }
+
+    // Print :--
+    public static void print() {
+        Node temp = head;
+
+        while(temp != null) { 
+            System.out.print(temp.data + "-> ");
+            temp = temp.next;
+        }
+        System.out.println("null");
+    }
+
+    //getMid :--
+    private Node getMid(Node head) {
         Node slow = head;
-        Node fast = head;
-        boolean cycle = false;
+        Node fast = head.next;
+
         while(fast != null && fast.next != null) {
             slow = slow.next;
             fast = fast.next.next;
-            if(fast == slow) {
-                cycle = true;
-                break;
+        }
+        return slow; // mid node
+    }
+
+    private Node merge(Node head1, Node head2) {
+        Node mergedLL = new Node(-1);
+        Node temp = mergedLL;
+
+        while(head1 != null && head2 != null) {
+            if(head1.data <= head2.data) {
+                temp.next = head1;
+                head1 = head1.next;
+                temp = temp.next;
+            } else {
+                temp.next = head2;
+                head2 = head2.next;
+                temp = temp.next;
             }
         }
-        if(cycle == false) {
-            return ;
+
+        while(head1 != null) {
+            temp.next = head1;
+            head1 = head1.next;
+            temp = temp.next;
         }
 
-        //find meeting point :--
-        slow = head;
-        Node prev = null; // last node
-        while (slow != fast) {
-            prev = fast;
-            slow = slow.next;
-            fast = fast.next;
+        while(head2 != null) {
+            temp.next = head2;
+            head2 = head2.next;
+            temp = temp.next;
         }
-
-        //remove cycle -> last.next = null
-        prev.next = null;
+        return mergedLL.next;
     }
- 
+    public Node mergeSort(Node head) {
+        if(head == null || head.next == null) {
+            return head;
+        }
+        //find mid
+        Node mid = getMid(head);
+
+        //left & right MS
+        Node rightHead = mid.next;
+        mid.next = null;
+        Node newLeft = mergeSort(head);
+        Node newRight = mergeSort(rightHead);
+
+        //merge
+        return merge (newLeft, newRight);
+    }
     public static void main(String args[]) {
-        // LinkedList ll = new LinkedList();
-        head = new Node(1);
-        Node temp = new Node(2);
-        head.next = temp;
-        head.next.next = new Node(3);
-        head.next.next.next = temp;
-        // 1->2->3->1
-         System.out.println(isCycle());
-         removeCycle();
-         System.out.println(isCycle());
+        MyLinkedList ll = new MyLinkedList();
+        ll.addFirst(1);
+        ll.addFirst(2);
+        ll.addFirst(3);
+        ll.addFirst(4);
+        ll.addFirst(5);
+        // 5->4->3->2->1
+
+        ll.print();
+        ll.head = ll.mergeSort(ll.head);
+        ll.print();
     }
- }
-
-// =======================================================================================================
-// =======================================================================================================
-
-
-
-
-
+}
 
 
 
