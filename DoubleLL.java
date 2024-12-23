1172fbdd7055030c8df35c64b48ac87ba9eaa482
Gautam-Rathour <gau321tam@gmail.com>
2024-12-23 15:01:18 +0530

    Add

---
+++ DoubleLL.java
@@ -0,0 +1,66 @@
+public class DoubleLL {
+    public class Node{
+        int data;
+        Node next;
+        Node prev;
+
+        public Node(int data) {
+            this.data = data;
+            this.next = null;
+            this.next = null;
+        }
+    }
+    public static  Node head;
+    public static  Node tail;
+    public static int size;
+
+    // addFirst --
+    public void addFirst(int data) {
+        Node newNode = new Node(data);
+        size++;
+        if(head == null) {
+            head = tail = newNode;
+            return;
+        }
+        
+        newNode.next = head;
+        head.prev = newNode;
+        head = newNode;
+    }
+
+    //print --
+    public void print() {
+        if(head == null) {
+            System.out.println("dll is empty");
+            return;
+        }
+        Node temp = head;
+
+        while(temp != null) {
+            System.out.print(temp.data + "<->");
+            temp = temp.next;
+        }
+        System.out.println("null");
+    }
+
+    // removeFirst --
+    public void removeFirst() {
+        size--;
+        if(head == null) {
+            System.out.println("dll is empty");
+            return;
+        }
+        head = head.next;
+        head.prev = head;
+    }
+
+    public static void main(String args []) {
+        DoubleLL dll = new DoubleLL();
+        dll.addFirst(3);
+        dll.addFirst(2);
+        dll.addFirst(1);
+
+        dll.print();
+        System.out.println(size);
+    }
+}
--- MyLinkedList.java
+++ MyLinkedList.java
@@ -471,164 +471,166 @@
 // =======================================================================================================
 // =======================================================================================================
 
-// merge sort in Linked-List : --
+// // merge sort in Linked-List : --
 
-import java.util.*;
-public class MyLinkedList {
-    public static class Node{
-        int data;
-        Node next;
+// import java.util.*;
+// public class MyLinkedList {
+//     public static class Node{
+//         int data;
+//         Node next;
 
-        public Node (int data) {
-            this.data = data;
-            this.next = null;
-        }
-    }
+//         public Node (int data) {
+//             this.data = data;
+//             this.next = null;
+//         }
+//     }
 
-    public static Node head;
-    // add-First :-
-    public static void addFirst(int data) {
-        Node newNode = new Node(data);
-        if(head == null) {
-            head = newNode;
-            return;
-        }
-        newNode.next = head;
-        head = newNode;
-    }
+//     public static Node head;
+//     // add-First :-
+//     public static void addFirst(int data) {
+//         Node newNode = new Node(data);
+//         if(head == null) {
+//             head = newNode;
+//             return;
+//         }
+//         newNode.next = head;
+//         head = newNode;
+//     }
 
-    // Print :--
-    public static void print() {
-        Node temp = head;
+//     // Print :--
+//     public static void print() {
+//         Node temp = head;
 
-        while(temp != null) { 
-            System.out.print(temp.data + "-> ");
-            temp = temp.next;
-        }
-        System.out.println("null");
-    }
+//         while(temp != null) { 
+//             System.out.print(temp.data + "-> ");
+//             temp = temp.next;
+//         }
+//         System.out.println("null");
+//     }
 
-    // //getMid :--
-    // private Node getMid(Node head) {
-    //     Node slow = head;
-    //     Node fast = head.next;
+//     // //getMid :--
+//     // private Node getMid(Node head) {
+//     //     Node slow = head;
+//     //     Node fast = head.next;
 
-    //     while(fast != null && fast.next != null) {
-    //         slow = slow.next;
-    //         fast = fast.next.next;
-    //     }
-    //     return slow; // mid node
-    // }
+//     //     while(fast != null && fast.next != null) {
+//     //         slow = slow.next;
+//     //         fast = fast.next.next;
+//     //     }
+//     //     return slow; // mid node
+//     // }
 
-    // private Node merge(Node head1, Node head2) {
-    //     Node mergedLL = new Node(-1);
-    //     Node temp = mergedLL;
+//     // private Node merge(Node head1, Node head2) {
+//     //     Node mergedLL = new Node(-1);
+//     //     Node temp = mergedLL;
 
-    //     while(head1 != null && head2 != null) {
-    //         if(head1.data <= head2.data) {
-    //             temp.next = head1;
-    //             head1 = head1.next;
-    //             temp = temp.next;
-    //         } else {
-    //             temp.next = head2;
-    //             head2 = head2.next;
-    //             temp = temp.next;
-    //         }
-    //     }
+//     //     while(head1 != null && head2 != null) {
+//     //         if(head1.data <= head2.data) {
+//     //             temp.next = head1;
+//     //             head1 = head1.next;
+//     //             temp = temp.next;
+//     //         } else {
+//     //             temp.next = head2;
+//     //             head2 = head2.next;
+//     //             temp = temp.next;
+//     //         }
+//     //     }
 
-    //     while(head1 != null) {
-    //         temp.next = head1;
-    //         head1 = head1.next;
-    //         temp = temp.next;
-    //     }
+//     //     while(head1 != null) {
+//     //         temp.next = head1;
+//     //         head1 = head1.next;
+//     //         temp = temp.next;
+//     //     }
 
-    //     while(head2 != null) {
-    //         temp.next = head2;
-    //         head2 = head2.next;
-    //         temp = temp.next;
-    //     }
-    //     return mergedLL.next;
-    // }
-    // public Node mergeSort(Node head) {
-    //     if(head == null || head.next == null) {
-    //         return head;
-    //     }
-    //     //find mid
-    //     Node mid = getMid(head);
+//     //     while(head2 != null) {
+//     //         temp.next = head2;
+//     //         head2 = head2.next;
+//     //         temp = temp.next;
+//     //     }
+//     //     return mergedLL.next;
+//     // }
+//     // public Node mergeSort(Node head) {
+//     //     if(head == null || head.next == null) {
+//     //         return head;
+//     //     }
+//     //     //find mid
+//     //     Node mid = getMid(head);
 
-    //     //left & right MS
-    //     Node rightHead = mid.next;
-    //     mid.next = null;
-    //     Node newLeft = mergeSort(head);
-    //     Node newRight = mergeSort(rightHead);
+//     //     //left & right MS
+//     //     Node rightHead = mid.next;
+//     //     mid.next = null;
+//     //     Node newLeft = mergeSort(head);
+//     //     Node newRight = mergeSort(rightHead);
 
-    //     //merge
-    //     return merge (newLeft, newRight);
-    // }
+//     //     //merge
+//     //     return merge (newLeft, newRight);
+//     // }
 
-    // Find ZigZag LinkedList :--
-    public void zigZag() {
-        //find mid 
-        Node slow = head;
-        Node fast = head.next;
-        while(fast != null && fast.next != null) {
-            slow = slow.next;
-            fast = fast.next.next;
-        }
-        Node mid = slow;
+//     // Find ZigZag LinkedList :--
+//     public void zigZag() {
+//         //find mid 
+//         Node slow = head;
+//         Node fast = head.next;
+//         while(fast != null && fast.next != null) {
+//             slow = slow.next;
+//             fast = fast.next.next;
+//         }
+//         Node mid = slow;
 
-        //reverse 2nd half
-        Node curr = mid.next;
-        mid.next = null;
-        Node prev = null;
-        Node next = null;
+//         //reverse 2nd half
+//         Node curr = mid.next;
+//         mid.next = null;
+//         Node prev = null;
+//         Node next = null;
 
-        while(curr != null) {
-            next = curr.next;
-            curr.next = prev;
-            prev = curr;
-            curr = next;
-        }
+//         while(curr != null) {
+//             next = curr.next;
+//             curr.next = prev;
+//             prev = curr;
+//             curr = next;
+//         }
         
-        Node left = head;
-        Node right = prev;
-        Node nextL, nextR;
+//         Node left = head;
+//         Node right = prev;
+//         Node nextL, nextR;
 
-        //alt merge - zig-zag merge
-        while(left != null && right != null) {
-            nextL = left.next;
-            left.next = right;
-            nextR = right.next;
-            right.next = nextL;
+//         //alt merge - zig-zag merge
+//         while(left != null && right != null) {
+//             nextL = left.next;
+//             left.next = right;
+//             nextR = right.next;
+//             right.next = nextL;
 
-            //Update: -
-            left = nextL;
-            right = nextR;
-        }
-    }
-    public static void main(String args[]) {
-        MyLinkedList ll = new MyLinkedList();
-        ll.addFirst(5);
-        ll.addFirst(4);
-        ll.addFirst(3);
-        ll.addFirst(2);
-        ll.addFirst(1);
-        // 5->4->3->2->1
+//             //Update: -
+//             left = nextL;
+//             right = nextR;
+//         }
+//     }
+//     public static void main(String args[]) {
+//         MyLinkedList ll = new MyLinkedList();
+//         ll.addFirst(5);
+//         ll.addFirst(4);
+//         ll.addFirst(3);
+//         ll.addFirst(2);
+//         ll.addFirst(1);
+//         // 5->4->3->2->1
 
-        // ll.print();
-        // ll.head = ll.mergeSort(ll.head);
-        // ll.print();
+//         // ll.print();
+//         // ll.head = ll.mergeSort(ll.head);
+//         // ll.print();
 
-        ll.print();
-        ll.zigZag();
-        ll.print();
-    }
-}
+//         ll.print();
+//         ll.zigZag();
+//         ll.print();
+//     }
+// }
 
 // ========================================================================================================
 // ========================================================================================================
 
+// Double LinkedList :--
 
+// Go to the next File ;
 
 
 
