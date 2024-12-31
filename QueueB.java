75acb8db793b5aa9459cafed46195963c3631a50
Gautam-Rathour <gau321tam@gmail.com>
2024-12-31 10:19:11 +0530

    Add

--- QueueB.java
+++ QueueB.java
@@ -470,35 +470,132 @@
 // =======================================================================================================
 // =======================================================================================================
 
-// Reverse a Queue :--
+// // Reverse a Queue :--
+
+// import java.util.*;
+// public class QueueB {
+//     public static void reverse(Queue<Integer> q) {
+//         Stack<Integer> s = new Stack<>();
+
+//         while(!q.isEmpty()) {
+//             s.push(q.remove());
+//         }
+
+//         while(!s.isEmpty()) {
+//             q.add(s.pop());
+//         }
+//     }
+
+//     public static void main(String args[]) {
+//         Queue<Integer> q = new LinkedList<>();
+//         q.add(1);
+//         q.add(2);
+//         q.add(3);
+//         q.add(4);
+//         q.add(5);
+
+//         reverse(q);
+
+//         while(!q.isEmpty()) {
+//             System.out.print(q.remove() + " ");
+//         }
+//         System.out.println();
+//     }
+// }
+
+// =======================================================================================================
+// =======================================================================================================
+
+// // Deque : --
+// // Deque is a double-ended queue. It is a queue that allows elements to be added or....
+
+// import java.util.*;
+// public class QueueB {
+//     public static void main(String args[]) {
+//         Deque<Integer> deque = new LinkedList<>();
+//         deque.addFirst(1);
+//         deque.addFirst(2);
+//         deque.addLast(3);
+//         deque.addLast(4);
+
+//         System.out.println(deque);;
+//         deque.removeLast();
+//         System.out.println(deque);
+
+//         System.out.println("First el = " + deque.getFirst());
+//         System.out.println("Last el = " + deque.getLast());
+//     }
+// }
+
+// =======================================================================================================
+// =======================================================================================================
+
+// // Stack & Queue using Deque :--
+
+// import java.util.*;
+// public class QueueB {
+//     static class Stack {
+//         Deque<Integer> deque = new LinkedList<>();
+
+//         public void push(int data) {
+//             deque.addLast(data);
+//         }
+
+//         public int pop() {
+//             return deque.removeLast();
+//         }
+
+//         public int  peek() {
+//             return deque.getLast();
+//         }
+//     }
+    
+
+//     public static void main (String args[]) {
+//         Stack s = new Stack();
+//         s.push(1);
+//         s.push(2);
+//         s.push(3);
+
+//         System.out.println("Peek = " + s.peek());
+//         System.out.println(s.pop());
+//         System.out.println(s.pop());
+//         System.out.println(s.pop());
+//     }
+// }
+
+// =======================================================================================================
+// =======================================================================================================
+
+// // Stack & Queue using Deque :--
 
 import java.util.*;
 public class QueueB {
-    public static void reverse(Queue<Integer> q) {
-        Stack<Integer> s = new Stack<>();
+    static class Queue {
+        Deque<Integer> deque = new LinkedList<>();
 
-        while(!q.isEmpty()) {
-            s.push(q.remove());
+        public void add(int data) {
+            deque.addLast(data);
         }
 
-        while(!s.isEmpty()) {
-            q.add(s.pop());
+        public int remove() {
+            return deque.removeFirst();
+        }
+
+        public int peek() {
+            return deque.getFirst();
         }
     }
-
-    public static void main(String args[]) {
-        Queue<Integer> q = new LinkedList<>();
+    public static void main (String args[]) {
+        Queue q = new Queue();
         q.add(1);
         q.add(2);
         q.add(3);
-        q.add(4);
-        q.add(5);
 
-        reverse(q);
 
-        while(!q.isEmpty()) {
-            System.out.print(q.remove() + " ");
-        }
-        System.out.println();
+        System.out.println("Peek = " + q.peek());
+        System.out.println(q.remove());
+        System.out.println(q.remove());
+        System.out.println(q.remove());
     }
 }
