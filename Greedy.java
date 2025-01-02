5f53a6adc9e3df89ceec762949f362f90b3fd746
Gautam-Rathour <gau321tam@gmail.com>
2025-01-02 12:25:38 +0530

    Add

--- Greedy.java
+++ Greedy.java
@@ -168,50 +168,59 @@
 // =======================================================================================================
 // =======================================================================================================
 
+// // Job Sequencing Problem :--
+
+// import java.util.*;
+// public class Greedy {
+//     static class Job {
+//         int deadline;
+//         int profit;
+//         int id;
+
+//         public Job(int i, int d, int p) {
+//             id = i;
+//             deadline = d;
+//             profit = p;
+//         }
+//     }
+//     public static void main(String args[]) {
+//         int jobsInfo[][] = {{4, 20}, {1, 10}, {1, 40}, {1, 30}};
+
+//         ArrayList<Job> jobs = new ArrayList<>();
+
+//         for(int i=0; i<jobsInfo.length; i++) {
+//             jobs.add(new Job(i, jobsInfo[i][0], jobsInfo[i][1]));
+//         }
+
+//         Collections.sort(jobs, (obj1, obj2) -> obj2.profit - obj1.profit); //descending
+//         // descending order of profit
+
+
+//         ArrayList<Integer> seq = new ArrayList<>();
+//         int time = 0;
+//         for(int i=0; i<jobs.size(); i++) {
+//             Job curr = jobs.get(i);
+//             if(curr.deadline > time) {
+//                 seq.add(curr.id);
+//                 time++;
+//             }
+//         }
+
+//         //print seq
+//         System.out.println("Max jobs = " + seq.size());
+//         for(int i=0; i<seq.size(); i++) {
+//             System.out.print(seq.get(i) + " ");
+//         }
+//         System.out.println();
+//     }
+// }
+
+// =======================================================================================================
+// =======================================================================================================
+
 import java.util.*;
 public class Greedy {
-    static class Job {
-        int deadline;
-        int profit;
-        int id;
-
-        public Job(int i, int d, int p) {
-            id = 1;
-            deadline = d;
-            profit = p;
-        }
-    }
-
-
     public static void main(String args[]) {
-        int jobsInfo[][] = {{4, 20}, {1, 10}, {1, 40}, {1, 30}};
-
-        ArrayList<Job> jobs = new ArrayList<>();
-
-        Job jobs[] = new Job[jobsInfo.length];
-
-        for(int i=0; i<jobs.length; i++) {
-            jobs[i] = new Jobo
-        }
-
-        Collections.sort(jobs, (obj1, obj,2) -> obj1.profit - obj1.profit); //descending
-        // descending order of profit
-
-        ArrayList<Integer> seq = new ArrayList<>();
-        int time = 0;
-        for(int i=0; i<jobs.size(); i++) {
-            Job curr = jobs.get(i);
-            if(curr.deadLine > time) {
-                seq.add(curr.id);
-                time++;
-            }
-        }
-
-        //print seq
-        System.out.println("Max jobs = " + seq.size());
-        for(int i=0; i<seq.size(); i++) {
-            System.out.println(seq.get(i) + " ");
-        }
-        System.out.println();
+        
     }
 }
