d97b990a99cedf4b267d40c8b4bad0c5a361e2ac
Gautam-Rathour <gau321tam@gmail.com>
2025-01-05 20:30:33 +0530

    Add

--- BinaryTreesB.java
+++ BinaryTreesB.java
@@ -242,7 +242,94 @@
 // =======================================================================================================
 // =======================================================================================================
 
-// Find Diameter of a Tree :----
+// // Find Diameter of a Tree :----
+
+// import java.util.*;
+// public class BinaryTreesB {
+//     static class Node {
+//         int data;
+//         Node left;
+//         Node right;
+
+//         Node (int data) {
+//             this.data = data;
+//             this.left = null;
+//             this.right = null;
+//         }
+//     }
+
+//     // public static int height(Node root) {
+//     //     if(root == null) {
+//     //         return 0;
+//     //     }
+//     //     int lh = height(root.left);
+//     //     int rh = height(root.right);
+//     //     return Math.max(lh, rh) + 1;
+//     // }
+
+//     // // Find Diameter  : ---
+//     // public static int diameter2(Node root) {   // O(n^2)
+//     //     if(root == null) {
+//     //         return 0;
+//     //     }
+
+//     //     int ld = diameter2(root.left);
+//     //     int lh = height(root.left);
+//     //     int rd = diameter2(root.right);
+//     //     int rh = height(root.right);
+
+//     //     int selfDiam = lh + rh + 1;
+
+//     //     return Math.max(selfDiam, Math.max(ld, rd));
+//     // }
+
+//     // Approach 2  : ---
+//     static class Info {
+//         int diam;
+//         int ht;
+
+//         public Info(int diam, int ht) {
+//             this.diam = diam;
+//             this.ht = ht;
+//         }
+//     }
+//     public static Info diameter(Node root) {  // O(n)
+//         if(root == null) {
+//             return new Info(0, 0);
+//         }
+
+//         Info leftInfo = diameter(root.left);
+//         Info rightInfo = diameter(root.right);
+
+//         int diam = Math.max(Math.max(leftInfo.diam, leftInfo.diam), rightInfo.ht + rightInfo.ht + 1);
+//         int ht = Math.max(leftInfo.ht, rightInfo.ht) + 1;
+
+//         return new Info(diam, ht);
+//     }
+
+//     public static void main(String args[]) {
+//             /* 
+//                          1
+//                         /  \
+//                        2    3
+//                       / \  / \
+//                      4   5 6  7
+//             */
+//             Node root = new Node(1);
+//             root.left = new Node (2);
+//             root.right = new Node (3);
+//             root.left.left = new Node(4);
+//             root.left.right = new Node(5);
+//             root.right.left = new Node(6);
+//             root.right.right = new Node (7);
+
+//             System.out.println(diameter2(root));
+//             // System.out.println(diameter(root).diam);
+//     }
+// }
+
+// =======================================================================================================
+// =======================================================================================================
 
 import java.util.*;
 public class BinaryTreesB {
@@ -251,60 +338,40 @@ public class BinaryTreesB {
         Node left;
         Node right;
 
-        Node (int data) {
+        Node(int data) {
             this.data = data;
             this.left = null;
             this.right = null;
         }
     }
 
-    public static int height(Node root) {
-        if(root == null) {
-            return 0;
+    public static boolean isIdentical(Node node, Node subRoot) {
+        if(node == null && subRoot == null) {
+            return true;
+        } else if(node == null || subRoot == null || node.data != subRoot.data) {
+            return false;
         }
-        int lh = height(root.left);
-        int rh = height(root.right);
-        return Math.max(lh, rh) + 1;
+
+        if(!isIdentical(node.left, subRoot.left)) {
+            return false;
+        }
+        if(!isIdentical(node.right, subRoot.right)) {
+            return false;
+        }
+
+        return true;
     }
-
-    // Find Diameter  : ---
-    public static int diameter2(Node root) {   // O(n^2)
+    public static boolean isSubtree(Node root, Node subRoot) {
         if(root == null) {
-            return 0;
+            return false;
+        }
+        if(root.data == subRoot.data) {
+            if(isIdentical(root, subRoot)) {
+                return true;
+            }
         }
 
-        int ld = diameter2(root.left);
-        int lh = height(root.left);
-        int rd = diameter2(root.right);
-        int rh = height(root.right);
-
-        int selfDiam = lh + rh + 1;
-
-        return Math.max(selfDiam, Math.max(ld, rd));
-    }
-
-    // Approach 2  : ---
-    static class Info {
-        int diam;
-        int ht;
-
-        public Info(int diam, int ht) {
-            this.diam = diam;
-            this.ht = ht;
-        }
-    }
-    public static Info diameter(Node root) {  // O(n)
-        if(root == null) {
-            return new Info(0, 0);
-        }
-
-        Info leftInfo = diameter(root.left);
-        Info rightInfo = diameter(root.right);
-
-        int diam = Math.max(Math.max(leftInfo.diam, leftInfo.diam), rightInfo.ht + rightInfo.ht + 1);
-        int ht = Math.max(leftInfo.ht, rightInfo.ht) + 1;
-
-        return new Info(diam, ht);
+        return isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot);
     }
 
     public static void main(String args[]) {
@@ -323,6 +390,15 @@ public class BinaryTreesB {
             root.right.left = new Node(6);
             root.right.right = new Node (7);
 
-            System.out.println(diameter(root).diam);
+            /* 
+                       2    
+                      / \ 
+                     4   5
+            */
+            Node subRoot = new Node(2);
+            subRoot.left = new Node(4);
+            // subRoot.right = new Node(5);
+
+            System.out.println(isSubtree(root, subRoot));
     }
 }
