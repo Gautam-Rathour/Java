f6d1fb73f084c80ac84805cbd742de8a60fe7031
Gautam-Rathour <gau321tam@gmail.com>
2025-01-12 12:12:11 +0530

    Add

--- BinarySearchTrees.java
+++ BinarySearchTrees.java
@@ -327,8 +327,101 @@
 // =======================================================================================================
 // =======================================================================================================
 
-    // Make a Balance BST  :: ---
-import java.util.*;
+//     // Make a Balance BST  :: ---
+// import java.util.*;
+// public class BinarySearchTrees {
+//     static class Node {
+//         int data;
+//         Node left;
+//         Node right;
+
+//         Node (int data) {
+//             this.data = data;
+//             this.left = null;
+//             this.right = null;
+//         }
+//     }
+//     public static void preorder(Node root) {
+//         if(root == null) {
+//             return;
+//         }
+//         System.out.print(root.data + " ");
+//         preorder(root.left);
+//         preorder(root.right);
+//     }
+
+//     // Make a Balance BST
+//     public static void getInorder(Node root, ArrayList<Integer> inorder) {
+//         if(root == null) {
+//             return;
+//         }
+//         getInorder(root.left, inorder);
+//         inorder.add(root.data);
+//         getInorder(root.right, inorder);
+//     }
+
+//     public static Node createBST(ArrayList<Integer> inorder, int start, int end) {
+//         if(start > end) {
+//             return null;
+//         }
+
+//         int mid = (start + end) / 2;
+//         Node root = new Node(inorder.get(mid));
+//         root.left = createBST(inorder, start, mid - 1);
+//         root.right = createBST(inorder, mid + 1, end);
+//         return root;
+//     }
+//     public static Node balanceBST(Node root) {   // - O(n)
+//         //inorder seq
+//         ArrayList<Integer> inorder = new ArrayList<> ();
+//         getInorder(root, inorder);
+
+//         // sorted inorder -> balanced BST
+//         root = createBST (inorder, 0, inorder.size()-1);
+//         return root;
+//     }
+
+//     public static void main(String args[]) {
+//         // int arr [] = {3, 5, 6, 8, 10, 11, 12};
+//         // int start = 0;
+//         // int end = arr.length-1;
+
+//         /*
+//                     8
+//                    / \
+//                   6   10
+//                  /      \
+//                 5        11
+//                /           \
+//               3             12
+//         given BST
+//          */
+
+//         Node root = new Node(8);
+//         root.left = new Node(6);
+//         root.left.left = new Node(5);
+//         root.left.left.left = new Node(3);
+
+//         root.right = new Node(10);
+//         root.right.right = new Node(11);
+//         root.right.right.right = new Node(12);
+//             /*
+//                             8
+//                            / \
+//                           5   11
+//                          / \   / \
+//                         3   6 10 12
+//                 expected BST
+
+//          */
+
+//         root = balanceBST(root);
+//         preorder(root);
+//     }
+// }
+// =======================================================================================================
+// =======================================================================================================
+
 public class BinarySearchTrees {
     static class Node {
         int data;
@@ -341,83 +434,72 @@ public class BinarySearchTrees {
             this.right = null;
         }
     }
-    public static void preorder(Node root) {
+    
+    static class Info {
+        boolean isBST;
+        int size;
+        int min;
+        int max;
+
+        public Info(boolean isBST, int size, int min, int max) {
+            this.isBST = isBST;
+            this.size = size;
+            this.min = min;
+            this.max = max;
+        }
+    }
+    public static int maxBST = 0;
+    public static Info largestBST(Node root) {
         if(root == null) {
-            return;
+            return new Info(true, 0, Integer.MAX_VALUE, Integer.MIN_VALUE);
         }
-        System.out.print(root.data + " ");
-        preorder(root.left);
-        preorder(root.right);
-    }
+        Info leftInfo = largestBST(root.left);
+        Info rightInfo = largestBST(root.right);
+        int size = leftInfo.size + rightInfo.size + 1;
+        int min = Math.min(root.data, Math.min(leftInfo.min, rightInfo.min));
+        int max = Math.max(root.data, Math.max(leftInfo.max, rightInfo.max));
 
-    // Make a Balance BST
-    public static void getInorder(Node root, ArrayList<Integer> inorder) {
-        if(root == null) {
-            return;
-        }
-        getInorder(root.left, inorder);
-        inorder.add(root.data);
-        getInorder(root.right, inorder);
-    }
-
-    public static Node createBST(ArrayList<Integer> inorder, int start, int end) {
-        if(start > end) {
-            return null;
+        if(root.data <= leftInfo.max || root.data >= rightInfo.min) {
+            return new Info(false, size, min, max);
         }
 
-        int mid = (start + end) / 2;
-        Node root = new Node(inorder.get(mid));
-        root.left = createBST(inorder, start, mid - 1);
-        root.right = createBST(inorder, mid + 1, end);
-        return root;
-    }
-    public static Node balanceBST(Node root) {
-        //inorder seq
-        ArrayList<Integer> inorder = new ArrayList<> ();
-        getInorder(root, inorder);
-
-        // sorted inorder -> balanced BST
-        root = createBST (inorder, 0, inorder.size()-1);
-        return root;
+        if(leftInfo.isBST && rightInfo.isBST) {
+            maxBST = Math.max(maxBST, size);
+            return new Info(true, size, min, max);
+        }
+        return new Info(false, size, min, max);
     }
 
     public static void main(String args[]) {
-        // int arr [] = {3, 5, 6, 8, 10, 11, 12};
-        // int start = 0;
-        // int end = arr.length-1;
-
-        /*
-                    8
-                   / \
-                  6   10
-                 /      \
-                5        11
-               /           \
-              3             12
-        given BST
-         */
-
-        Node root = new Node(8);
-        root.left = new Node(6);
+        /*               50
+                        /  \
+                       30   60
+                    /   \   / \
+                   5    20 45 70
+                           /    \
+                         65      80
+                given BST
+ */
+        Node root = new Node(50);
+        root.left = new Node(30);
         root.left.left = new Node(5);
-        root.left.left.left = new Node(3);
+        root.left.right = new Node (20);
 
-        root.right = new Node(10);
-        root.right.right = new Node(11);
-        root.right.right.right = new Node(12);
-            /*
-                            8
-                           / \
-                          5   11
-                         / \   / \
-                        3   6 10 12
-                expected BST
+        root.right = new Node(60);
+        root.right.left = new Node(45);
+        root.right.right = new Node(70);
+        root.right.right.left = new Node(65);
+        root.right.right.right = new Node(80);
 
-         */
+                /*            60
+                             / \
+                            45 70
+                           /     \
+                          65     80 
+            expected BST  : size  = 5
+                */
 
-        root = balanceBST(root);
-        preorder(root);
+        Info info = largestBST (root);
+        System.out.println("largest BST size = " + maxBST);
     }
 }
-// =======================================================================================================
-// =======================================================================================================
