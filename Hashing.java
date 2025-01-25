beab9c840a004e5e53d5004d3ba872a940f38238
Gautam-Rathour <gau321tam@gmail.com>
2025-01-25 10:43:21 +0530

    Add

--- Hashing.java
+++ Hashing.java
@@ -397,26 +397,95 @@
 // =======================================================================================================
 // =======================================================================================================
 
-//
+// // LinkedHashSet :  ---
 
-import java.util.*;
-public class Hashing {
+// import java.util.*;
+// public class Hashing {
+//     public static void main(String args[]) {
+//         HashSet<String> cities = new HashSet<>();
+//         cities.add("Delhi");
+//         cities.add("Mumbai");
+//         cities.add("Noida");
+//         cities.add("Bengalure");
+//         System.out.println(cities);
 
-    public static void main(String args[]) {
-        HashSet<String> cities = new HashSet<>();
-        cities.add("Delhi");
-        cities.add("Mumbai");
-        cities.add("Noida");
-        cities.add("Bengalure");
-        System.out.println(cities);
+//         LinkedHashSet<String> lhs = new LinkedHashSet<>();
+//         lhs.add("Delhi");
+//         lhs.add("Mumbai");
+//         lhs.add("Noida");
+//         lhs.add("Bengalure");
+//         System.out.println(lhs);
 
-        LinkedHashSet<String> lhs = new LinkedHashSet<>();
-        lhs.add("Delhi");
-        lhs.add("Mumbai");
-        lhs.add("Noida");
-        lhs.add("Bengalure");
-        System.out.println(lhs);
+//         // Tree - Set : ---- ( Sorted format ) -
+//         TreeSet<String> ts = new TreeSet<>();
+//         ts.add("Delhi");
+//         ts.add("Mumbai");
+//         ts.add("Noida");
+//         ts.add("Bengalure");
+//         System.out.println(ts);
 
      
-    }
-}
+//     }
+// }
+
+// =======================================================================================================
+// =======================================================================================================
+
+// // Count Distinct Elements  :  ---
+
+// import java.util.*;
+// public class Hashing {
+//     public static void main(String args[]) {
+//         int num[] = {4, 3, 2, 5, 6, 7, 3, 4, 2, 1};
+//         HashSet<Integer> set = new HashSet<>();
+
+//         for(int i=0; i<num.length; i++) {
+//             set.add(num[i]);
+//         }
+
+//         System.out.println("Ans = " + set.size());
+//     }
+// }
+
+// =======================================================================================================
+// =======================================================================================================
+
+// // Union & Intersection of 2 arrays  : ---
+// import java.util.*;
+// public class Hashing {
+//     public static void main(String args[]) {
+//         int arr1[] = {7, 3, 9};
+//         int arr2[] = {6, 3, 9, 2, 9, 4};
+//         HashSet<Integer> set = new HashSet<>();
+
+//         //  Union
+//         for(int i=0; i<arr1.length; i++) {
+//             set.add(arr1[i]);
+//         }
+//         for(int i=0; i<arr2.length; i++) {
+//             set.add(arr2[i]);
+//         }
+
+//         System.out.println("Union = " + set.size());
+
+//         // Intersection
+//         set.clear();
+//         for(int i=0; i<arr1.length; i++) {
+//             set.add(arr1[i]);
+//         }
+//         int count = 0;
+//         for(int i=0; i<arr2.length; i++) {
+//             if(set.contains(arr2[i])) {
+//                 count++;
+//                 set.remove(arr2[i]);
+//             }
+//         }
+//         System.out.println("Intersection = " + count);
+//     }
+// }
+
+// =======================================================================================================
+// =======================================================================================================
+
+
+// =======================================================================================================
