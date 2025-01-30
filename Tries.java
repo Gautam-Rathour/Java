81d503793693e65c7bf3f3983b7ec586ddeb6746
Gautam-Rathour <gau321tam@gmail.com>
2025-01-30 15:38:49 +0530

    Add some Tries Code

--- Tries.java
+++ Tries.java
@@ -79,67 +79,142 @@
 // ========================================================================================================
 // ========================================================================================================
  
-public class Tries {
+// //  Find Prefix  :  -- 
 
+// public class Tries {
+
+//     static class Node {
+//         Node[] children = new Node[26];  // 'a' to 'z'
+//         boolean eow = false;
+//         int freq;
+
+//         public Node() {
+//             for(int i=0; i<children.length; i++) {
+//                 children[i] = null;
+//             }
+//             freq = 1;
+//         }
+//     }
+
+//     public static Node root = new Node();
+
+//     public static void insert(String word) {
+//         Node curr = root;
+//         for(int i=0; i<word.length(); i++) {
+//             int idx = word.charAt(i) - 'a';
+//             if(curr.children[idx] == null) {
+//                 curr.children[idx] = new Node();
+//             } else {
+//                 curr.children[idx].freq++;
+//             }
+//             curr = curr.children[idx];
+//         }
+
+//         curr.eow = true;
+//     }
+
+//     public static void findPrefix(Node root, String ans) { //O(L)
+//         if(root == null) {
+//             return;
+//         }
+
+//         if(root.freq == 1) {
+//             System.out.println(ans);
+//             return;
+//         }
+
+//         for(int i=0; i<root.children.length; i++) {
+//             if(root.children[i] != null) {
+//                 findPrefix(root.children[i], ans+(char)(i+'a'));
+//             }
+//         }
+//     }
+
+//     public static void main(String args[]) {
+//         String arr[] = {"zebra", "dog", "duck", "dove"};
+//         for(int i=0; i<arr.length; i++) {
+//             insert(arr[i]);
+//         }
+
+//         root.freq = -1;
+
+//         findPrefix(root, "");
+//     }
+// }
+
+// ========================================================================================================
+// ========================================================================================================
+ 
+//  Starts With Prefix  :  ----
+
+import java.util.*;
+public class Tries {
     static class Node {
-        Node[] children = new Node[26];  // 'a' to 'z'
-        boolean eow = false;
-        int freq;
+        Node children[] = new Node[26]; //'a' - 'z'
+        boolean isEndOfWord;
 
         public Node() {
-            for(int i=0; i<children.length; i++) {
+            for(int i=0; i<26; i++) {
                 children[i] = null;
             }
-            freq = 1;
         }
     }
 
     public static Node root = new Node();
 
+    //  Insert  :  --
     public static void insert(String word) {
         Node curr = root;
-        for(int i=0; i<word.length(); i++) {
-            int idx = word.charAt(i) - 'a';
-            if(curr.children[idx] == null) {
+        for(int level=0; level<word.length(); level++) {
+            int idx = word.charAt(level) - 'a';
+            if(curr.children[idx] ==  null) {
                 curr.children[idx] = new Node();
-            } else {
-                curr.children[idx].freq++;
             }
             curr = curr.children[idx];
         }
 
-        curr.eow = true;
+        curr.isEndOfWord = true;   
     }
 
-    public static void findPrefix(Node root, String ans) { //O(L)
-        if(root == null) {
-            return;
-        }
-
-        if(root.freq == 1) {
-            System.out.println(ans);
-            return;
-        }
-
-        for(int i=0; i<root.children.length; i++) {
-            if(root.children[i] != null) {
-                findPrefix(root.children[i], ans+(char)(i+'a'));
+    //  Search  : ---
+    public static boolean search(String key) {
+        Node curr = root;
+        for(int level=0; level<key.length(); level++) {
+            int idx = key.charAt(level) - 'a';
+            if(curr.children[idx] ==  null) {
+                return false;
             }
+            curr = curr.children[idx];
         }
+
+        return curr.isEndOfWord = true;
     }
 
-    public static void main(String args[]) {
-        String arr[] = {"zebra", "dog", "duck", "dove"};
-        for(int i=0; i<arr.length; i++) {
-            insert(arr[i]);
+    // Starts With Prefix
+    public static boolean startsWith(String prefix) {  // O(L)
+        Node curr = root;
+
+        for(int i=0; i<prefix.length(); i++) {
+            int idx = prefix.charAt(i) - 'a';
+            if(curr.children[idx] == null) {
+                return false;
+            }
+
+            curr = curr.children[idx];
         }
 
-        root.freq = -1;
+        return true;
+    }
+    public static void main(String args[]) {
+        String words[] = {"apple", "app", "mango", "man", "woman"};
+        String prefix1 = "app";
+        String prefix2 = "moon";
 
-        findPrefix(root, "");
+        for(int i=0; i<words.length; i++) {
+            insert(words[i]);
+        }
+
+        System.out.println(startsWith(prefix1));
+        System.out.println(startsWith(prefix2));
     }
 }
-
-// ========================================================================================================
-// ========================================================================================================
- 
