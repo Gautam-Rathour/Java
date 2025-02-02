ec4fe4302e0f6aa0988e8262041a0f935154a77d
Gautam-Rathour <gau321tam@gmail.com>
2025-02-02 23:39:28 +0530

    Add

--- Graphs.java
+++ Graphs.java
@@ -451,3 +451,45 @@
 // ==========================================================================================================
 
 // 2.  Live Lecture  :  --- 
+
+import java.util.*;
+public class Graphs {
+    static boolean cycleUsingBfs(int source, ArrayList<ArrayList<Integer>> adj, boolean isVisited[]) {
+        Queue <Integer> q = new LinkedList<>();
+        q.add( source );
+        isVisited[source] = true;
+
+        while(q.isEmpty() == false) {
+            int curr = q.remove();
+
+            for(int conn : adj.get(curr)) {
+                if(isVisited[conn] == true) {
+                    //there is a cycle
+                    return true;
+                } else {
+                    isVisited[conn] = true;
+                    q.add(conn);
+                }
+            }
+        }
+        // no cycle
+        return false;
+    }
+
+    static boolean detect(ArrayList<ArrayList<Integer>> adj, booealn isVisited[]) {
+        int n = adj.size();
+        for(int i=0; i<n; i++) {
+            if(isVisited[i] == false) {
+
+                boolean result = cycleUsingBfs(i, adj, isVisited);
+                if(result == true) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+    public static void main(String args[]) {
+
+    }
+}
