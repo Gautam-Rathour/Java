1d31772ae92ec33dabab4d6f432cc8dac5012c14
Gautam-Rathour <gau321tam@gmail.com>
2025-02-11 10:42:57 +0530

    Add Kosaraju's Algorithm Code

--- Graphs/Graphs5.java
+++ Graphs/Graphs5.java
@@ -99,54 +99,229 @@
 // ==========================================================================================================
 // ==========================================================================================================
 
-// Connecting Cities  :  -----
+// // Connecting Cities  :  -----
+
+// import java.util.*;
+// public class Graphs5 {
+//     static class Edge implements Comparable<Edge> {
+//         int dest;
+//         int cost;
+
+//         public Edge (int d, int c) {
+//             this.dest = d;
+//             this.cost = c;
+//         }
+
+//         @Override
+//         public int compareTo(Edge e2) {
+//             return this.cost - e2.cost; //ascending
+//         }
+//     }
+
+//     // prim's algorithm ----
+//     public static int connectCities(int cities[][]) {
+//         PriorityQueue<Edge> pq = new PriorityQueue<>();
+//         boolean vis[] = new boolean[cities.length];
+
+//         pq.add(new Edge(0, 0));
+//         int finalCost = 0;
+
+//         while(!pq.isEmpty()) {
+//             Edge curr = pq.remove();
+//             if(!vis[curr.dest]) {
+//                 vis[curr.dest] = true;
+//                 finalCost += curr.cost;
+
+//                 for(int i=0; i<cities[curr.dest].length; i++) {
+//                     if(cities[curr.dest][i] != 0) {
+//                         pq.add(new Edge(i, cities[curr.dest][i]));
+//                     }
+//                 }
+//             }
+//         }
+
+//         return finalCost;
+//     }
+//     public static void main(String args[]) {
+//         int cities[][] = {{0, 1, 2, 3, 4},
+//                           {1, 0, 5, 0, 7},
+//                           {2, 5, 0, 6, 0},
+//                           {3, 0, 6, 0, 0},
+//                           {4, 7, 0, 0, 0}};
+
+//         System.out.println(connectCities(cities));
+//     }
+// }
+
+// ==========================================================================================================
+// ==========================================================================================================
+
+// // Disjoint set Uinon : ---
+
+// import java.util.*;
+// public class Graphs5 {
+//     static int n = 7;
+//     static int par[] = new int[n];
+//     static int rank[] = new int[n];
+
+//     public static void init() {
+//         for(int i=0; i<n; i++) {
+//             par[i] = i;
+//         }
+//     }
+
+//     public static int find(int x) {
+//         if(x == par[x]) {
+//             return x;
+//         }
+//         return par[x] = find(par[x]);
+//     }
+//     public static void union(int a, int b) {
+//         int parA = find(a);
+//         int parB = find(b);
+
+//         if(rank[parA] == rank[parB]) {
+//             par[parB] = parA;
+//             rank[parA]++;
+//         } else if(rank[parA] < rank[parB]){
+//             par[parA] = parB;
+//         } else {
+//             par[parB] = parA;
+//         }
+//     }
+//     public static void main(String args[]) {
+//         init();
+//         union(1, 3);
+//         System.out.println(find(3));
+//         union(2, 4);
+//         union(3, 6);
+//         union(1, 4);
+//         System.out.println(find(3));
+//         System.out.println(find(4));
+//         union(1, 5);
+//     }
+// }
+
+// ==========================================================================================================
+// ==========================================================================================================
+
+// Kruskal's Algorithm  :  -------
 import java.util.*;
 public class Graphs5 {
     static class Edge implements Comparable<Edge> {
+        int src;
         int dest;
-        int cost;
+        int wt;
 
-        public Edge (int d, int c) {
+        public Edge(int s, int d, int w) {
+            this.src = s;
             this.dest = d;
-            this.cost = c;
+            this.wt = w;
         }
-
         @Override
         public int compareTo(Edge e2) {
-            return this.cost - e2.cost; //ascending
+            return this.wt - e2.wt;
+        }
+    }
+    static void createGraph(ArrayList<Edge> edges) {
+        // Edges
+        edges.add(new Edge(0, 1, 10));
+        edges.add(new Edge(0, 2, 15));
+        edges.add(new Edge(0, 3, 30));
+        edges.add(new Edge(1, 3, 40));
+        edges.add(new Edge(2, 3, 50));
+    }
+
+    static int n = 4;
+    static int par[] = new int[n];
+    static int rank[] = new int[n];
+
+    public static void init() {
+        for(int i=0; i<n; i++) {
+            par[i] = i;
+        }
+    }
+    
+    public static int find(int x) {
+        if(par[x] == x) {
+            return x;
+        }
+        return par[x] = find(par[x]);
+    }
+
+        public static void union(int a, int b) {
+        int parA = find(a);
+        int parB = find(b);
+
+        if(rank[parA] == rank[parB]) {
+            par[parB] = parA;
+            rank[parA]++;
+        } else if(rank[parA] < rank[parB]){
+            par[parA] = parB;
+        } else {
+            par[parB] = parA;
         }
     }
 
-    public static int connectCities(int cities[][]) {
-        PriorityQueue<Edge> pq = new PriorityQueue<>();
-        boolean vis[] = new boolean[cities.length];
+    // Kruskal's
+    public static void kruskalsMST(ArrayList<Edge> edges, int V) {
+        init();
+        Collections.sort(edges); // O(ElogE)
+        int mstCost = 0;
+        int count = 0;
 
-        pq.add(new Edge(0, 0));
-        int finalCost = 0;
+        for(int i=0; count<V-1; i++) {
+            Edge e = edges.get(i);
+            //(src, dest, wt)
 
-        while(!pq.isEmpty()) {
-            Edge curr = pq.remove();
-            if(!vis[curr.dest]) {
-                vis[curr.dest] = true;
-                finalCost += curr.cost;
-
-                for(int i=0; i<cities[curr.dest].length; i++) {
-                    if(cities[curr.dest][i] != 0) {
-                        pq.add(new Edge(i, cities[curr.dest][i]));
-                    }
-                }
+            int parA = find(e.src); // src = a
+            int parB = find(e.dest); //dest = b
+            if(parA != parB) {
+                union(e.src, e.dest);
+                mstCost += e.wt;
+                // count++;
             }
         }
+        
+        System.out.println("MST Cost =  " + mstCost);
+    }
+    public static void main (String args[]){
+        int V = 4;
+        ArrayList<Edge> edges = new ArrayList<>();
+        createGraph(edges);
 
-        return finalCost;
+        kruskalsMST(edges, V);
+    }
+}
+
+// ==========================================================================================================
+// ==========================================================================================================
+
+// Flood Fill Algorithm  : ----
+import java.util.*;
+public class Graphs5 {
+    // O(m*n)
+    public void helper(int[][] image, int sr, int sc, int color, boolean vis[][], int orgCol) {
+        if(sr < 0 || sc < 0 || sr >= image.length || sc >= image[0].length 
+        || vis[sr][sc] || image[sr][sc] != orgCol) {
+            return ;
+
+            // left
+            helper(image, sr, sc-1, color, vis, orgCol);
+            //right
+            helper(image, sr, sc+1, color, vis, orgCol);
+            //up
+            helper(image, sr-1, sc, color, vis, orgCol);
+            //down
+            helper(image, sr+1, sc, color, vis, orgCol);
+        }
+    }
+    public int [][] floodFill(int[][] image, int sr, int sc, int color) {
+        boolean vis[][] = new boolean[image.length][image[0].length];
+        helper(image, sr, sc, color, vis, image[sr][sc]);
+        return image;
     }
     public static void main(String args[]) {
-        int cities[][] = {{0, 1, 2, 3, 4},
-                          {1, 0, 5, 0, 7},
-                          {2, 5, 0, 6, 0},
-                          {3, 0, 6, 0, 0},
-                          {4, 7, 0, 0, 0}};
 
-        System.out.println(connectCities(cities));
     }
 }
