6dd45a397f0b24ec561239294517462d39363de0
Gautam-Rathour <gau321tam@gmail.com>
2025-02-12 23:43:03 +0530

    Add

--- Dynamic_Programming/DP2.java
+++ Dynamic_Programming/DP2.java
@@ -134,49 +134,91 @@
 // =======================================================================================================
 // =======================================================================================================
 
-// Target Sum Subset ( Code )  :  ----
+// // Target Sum Subset ( Code )  :  ----
+
+// import java.util.*;
+// public class DP2 {
+
+//     public static void print(boolean dp[][]) {
+//         for(int i=0; i<dp.length; i++) {
+//             for(int j=0; j<dp[0].length; j++) {
+//                 System.out.print(dp[i][j] + " ");
+//             }
+//             System.out.println();
+//         }
+//         System.out.println();
+//     }
+
+//     public static boolean targetSumSubset(int arr[], int sum) {
+//         int n = arr.length;
+//         boolean dp[][] = new boolean[n+1][sum+1];
+//         //i = items & j = target sum
+//         for(int i=0; i<n+1; i++) {
+//             dp[i][0] = true;
+//         }
+
+//         for(int i=1; i<n+1; i++) {
+//             for(int j=1; j<sum+1; j++) {
+//                 int v = arr[i-1];
+//                 //include
+//                 if(v <= j && dp[i-1][j-v] == true) {
+//                     dp[i][j] = true;
+//                 }
+//                 //exclude
+//                 else if(dp[i-1][j] == true) {
+//                     dp[i][j] = true;
+//                 }
+//             }
+//         }
+//         print(dp);
+//         return dp[n][sum];
+//     }
+//     public static void main(String args[]) {
+//         int arr[] = {4, 2, 7, 1, 3};
+//         int sum = 10;
+
+//         System.out.println(targetSumSubset(arr, sum));
+//     }
+// }
+
+// =======================================================================================================
+// =======================================================================================================
+
+// Unbounded Knapsack ( Tabulation )  :  ----
+// 1. We can include the item any number of times.
 
 import java.util.*;
 public class DP2 {
 
-    public static void print(boolean dp[][]) {
-        for(int i=0; i<dp.length; i++) {
-            for(int j=0; j<dp[0].length; j++) {
-                System.out.print(dp[i][j] + " ");
-            }
-            System.out.println();
-        }
-        System.out.println();
-    }
+    //O(n * w)
+    public static int unboundedKnapsack(int val[], int wt[], int W) {
+        int n = val.length;
+        int dp[][] = new int[n+1][W+1];
 
-    public static boolean targetSumSubset(int arr[], int sum) {
-        int n = arr.length;
-        boolean dp[][] = new boolean[n+1][sum+1];
-        //i = items & j = target sum
         for(int i=0; i<n+1; i++) {
-            dp[i][0] = true;
+            dp[i][0] = 0;
+        }
+        for(int j=0; j<W+1; j++) {
+            dp[0][j] = 0;
         }
 
         for(int i=1; i<n+1; i++) {
-            for(int j=1; j<sum+1; j++) {
-                int v = arr[i-1];
-                //include
-                if(v <= j && dp[i-1][j-v] == true) {
-                    dp[i][j] = true;
-                }
-                //exclude
-                else if(dp[i-1][j] == true) {
-                    dp[i][j] = true;
+            for(int j=1; j<W+1; j++) {
+                if(wt[i-1] <= j) { //valid
+                    dp[i][j] = Math.max(val[i-1] + dp[i][j-wt[i-1]], dp[i-1][j]);
+                } else { // inValid
+                    dp[i][j] = dp[i-1][j];
                 }
             }
         }
-        print(dp);
-        return dp[n][sum];
-    }
-    public static void main(String args[]) {
-        int arr[] = {4, 2, 7, 1, 3};
-        int sum = 10;
 
-        System.out.println(targetSumSubset(arr, sum));
+        return dp[n][W];
+    }
+    public static void  main(String args[]) {
+        int val[] = {15, 14, 10, 45, 30};
+        int wt[] = {2, 5, 1, 3, 4};
+        int W = 7;
+
+        System.out.println(unboundedKnapsack(val, wt, W));
     }
 }
