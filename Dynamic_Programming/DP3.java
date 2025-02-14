c6a60c5b840d7d960151de9c6fbee574024fc46b
Gautam-Rathour <gau321tam@gmail.com>
2025-02-15 00:59:00 +0530

    Add

--- Dynamic_Programming/DP3.java
+++ Dynamic_Programming/DP3.java
@@ -41,46 +41,68 @@
 // =======================================================================================================
 // =======================================================================================================
 
-// Rod Cutting : ----
+// // Rod Cutting : ----
+
+// public class DP3 {
+//     // weight => length; val => price; W => totRed
+//     public static int rodCutting(int length[], int price[], int totRod) {
+//         int n = price.length;
+//         int dp[][] = new int[n+1][totRod+1];
+
+//         for(int i=0; i<n+1; i++) {
+//             for(int j=0; j<totRod+1; j++) {
+//                 if(i == 0 || j == 0) {
+//                     dp[i][j] = 0;
+//                 }
+//             }
+//         }
+
+//         for(int i=1; i<n+1; i++) {
+//             for(int j=1; j<totRod+1; j++) {
+//                 //Valid
+//                 if(length[i-1] <= j) {
+//                     //dp[i][j] = Math.max(val[i-1] + dp[i][j-wt[i-1]], dp[i-1][j]);
+//                     dp[i][j] = Math.max(price[i-1] + dp[i][j-length[i-1]], dp[i-1][j]);
+//                 }
+//                 //inValid
+//                 else {
+//                     dp[i][j] = dp[i-1][j];
+//                 }
+//             }
+//         }
+
+//         return dp[n][totRod];
+//     }
+//     public static void main(String args[]) {
+//         int length[] = {1, 2, 3, 4, 5, 6, 7, 8}; // pieces length
+//         int price[] = {1, 5, 8, 9, 10, 17, 17, 20};
+//         int totRod = 8;
+
+//         System.out.println(rodCutting(length, price, totRod));
+//     }
+// }
+
+// =======================================================================================================
+// =======================================================================================================
 
 public class DP3 {
-    // weight => length; val => price; W => totRed
-    public static int rodCutting(int length[], int price[], int totRod) {
-        int n = price.length;
-        int dp[][] = new int[n+1][totRod+1];
-
-        for(int i=0; i<n+1; i++) {
-            for(int j=0; j<totRod+1; j++) {
-                if(i == 0 || j == 0) {
-                    dp[i][j] = 0;
-                }
-            }
+    public static int lcs(String str1, String str2, int n, int m) {
+        if(n == 0 || m == 0) {
+            return 0;
         }
 
-        for(int i=1; i<n+1; i++) {
-            for(int j=1; j<totRod+1; j++) {
-                //Valid
-                if(length[i-1] <= j) {
-                    //dp[i][j] = Math.max(val[i-1] + dp[i][j-wt[i-1]], dp[i-1][j]);
-                    dp[i][j] = Math.max(price[i-1] + dp[i][j-length[i-1]], dp[i-1][j]);
-                }
-                //inValid
-                else {
-                    dp[i][j] = dp[i-1][j];
-                }
-            }
+        if(str1.charAt(n-1) == str2.charAt(m-1)) {
+            return lcs(str1, str2, n-1, m-1) + 1;
+        } else {
+            int ans1 = lcs(str1, str2, n-1, m);
+            int ans2 = lcs(str1, str2, n, m-1);
+            return Math.max(ans1, ans2);
         }
-
-        return dp[n][totRod];
     }
     public static void main(String args[]) {
-        int length[] = {1, 2, 3, 4, 5, 6, 7, 8}; // pieces length
-        int price[] = {1, 5, 8, 9, 10, 17, 17, 20};
-        int totRod = 8;
+        String str1 = "abcdge";
+        String str2 = "abedg"; // lcs = "abdg";  length = 4
 
-        System.out.println(rodCutting(length, price, totRod));
+        System.out.println(lcs(str1, str2, str1.length(), str2.length()));
     }
 }
-
-// =======================================================================================================
-// =======================================================================================================
