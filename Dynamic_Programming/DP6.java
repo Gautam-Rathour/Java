f51009911e38de48513d917e0f7192347ede15ce
Gautam-Rathour <gau321tam@gmail.com>
2025-02-19 22:56:26 +0530

    Add

--- Dynamic_Programming/DP6.java
+++ Dynamic_Programming/DP6.java
@@ -117,7 +117,73 @@
 // =======================================================================================================
 // =======================================================================================================
 
-//
+// // Minumum Partitioning  :  --------
 
+// public class DP6 {
+//     public static int minPartition(int arr[]) {
+//         int n = arr.length;
+//         int sum = 0;
+//         for(int i=0; i<arr.length; i++) {
+//             sum += arr[i];
+//         }
 
+//         int W = sum/2;
+
+//         int dp[][] = new int[n+1][W+1];
+
+//         //bootom up
+//         for(int i=1; i<n+1; i++) {
+//             for(int j=1; j<W+1; j++) {
+//                 if(arr[i-1] <= j) {  // valid
+//                     dp[i][j] = Math.max(arr[i-1] + dp[i-1][j-arr[i-1]], dp[i-1][j]);
+//                 } else {  // invalid
+//                     dp[i][j] = dp[i-1][j];
+//                 }
+//             }
+//         }
+
+//         int sum1 = dp[n][W];
+//         int sum2 = sum - sum1;
+//         return Math.abs(sum1 - sum2);
+//     }
+//     public static void main(String arga[]) {
+//         int nums[] = {1, 6, 11, 5};
+
+//         System.out.println(minPartition(nums));
+//     }
+// }
+
+// =======================================================================================================
+// =======================================================================================================
+
+// Min Array Jumps  :  --------
+
+import java.util.*;
+public class DP6 {
+    public static int minJumps(int nums[]) {
+        int n = nums.length;
+        int dp[] = new int[n];
+        Arrays.fill(dp, -1);
+        dp[n-1] = 0;
+
+        for(int i=n-2; i>=0; i--) {
+            int steps = nums[i];
+            int ans = Integer.MAX_VALUE;
+            for(int j=i+1; j<=i+steps && j<n; j++) {
+                if(dp[j] != -1) {
+                    ans = Math.min(ans, dp[j]+1);
+                }
+            }
+            if(ans != Integer.MAX_VALUE) {
+                dp[i] = ans;
+            }
+        }
+        return dp[0];
+    }
+    public static void main(String args[]) {
+        int nums[] = {2, 3, 1, 1, 4};
+
+        System.out.println(minJumps(nums));
+    }
+}
 
