55e5c9058fce94fdf81fba4d38ab413e0a7b2992
Gautam-Rathour <gau321tam@gmail.com>
2025-03-04 23:07:48 +0530

    Add

---
+++ tempCodeRunnerFile.java
@@ -0,0 +1,11 @@
+     // while(h < costHor.length) {
+        //     cost += (costHor[h] * vp);
+        //     hp++;
+        //     h++;
+        // }
+
+        // while(v < costVer.length) {
+        //     cost += (costVer[v] * hp);
+        //     vp++;
+        //     v++;
+        // }
