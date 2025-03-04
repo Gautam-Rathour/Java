23d1d6ad6ca131cc0b6260406b6de9f9f5dd453b
Gautam-Rathour <gau321tam@gmail.com>
2025-03-04 12:56:18 +0530

    Chocola Problem

--- Greedy.java
+++ Greedy.java
@@ -388,31 +388,152 @@
 // =======================================================================================================
 // =======================================================================================================
  
-// Max Length Chain of Pairs  : --
-import java.util.*;
-public class Greedy {
-    public static void main(String args[]) {//O(nlog n)
-        int pairs[][] = {{5, 24}, {39, 60}, {5, 28}, {27,40}, {50, 90}};
+// // Max Length Chain of Pairs  : --
+// import java.util.*;
+// public class Greedy {
+//     public static void main(String args[]) {//O(nlog n)
+//         int pairs[][] = {{5, 24}, {39, 60}, {5, 28}, {27,40}, {50, 90}};
          
-        int pair = 0;
-        Arrays.sort(pairs, Comparator.comparingDouble(o -> o[1]));
+//         int pair = 0;
+//         Arrays.sort(pairs, Comparator.comparingDouble(o -> o[1]));
 
-        int chainLen = 1;
-        int chainEnd = pairs[0][1]; //Last selected pair end   //chain end
+//         int chainLen = 1;
+//         int chainEnd = pairs[0][1]; //Last selected pair end   //chain end
 
-        for(int i=1; i<pairs.length; i++) {
-            if(pairs[i][0] > chainEnd) {
-                chainLen++;
-                chainEnd = pairs[i][1];
-            }
-        }
+//         for(int i=1; i<pairs.length; i++) {
+//             if(pairs[i][0] > chainEnd) {
+//                 chainLen++;
+//                 chainEnd = pairs[i][1];
+//             }
+//         }
 
-        System.out.println("Max Chain Length = " + chainLen);
-    }
-}
+//         System.out.println("Max Chain Length = " + chainLen);
+//     }
+// }
 
 // =======================================================================================================
 // =======================================================================================================
  
+// // Greedy in Indian Coins  :  -----
 
+// import java.util.*;
+// public class Greedy {
+//     public static void main(String args[]) {
+//         Integer coins[] = {1, 2, 5, 10, 20, 50, 100, 500, 2000};
 
+//         Arrays.sort(coins, Comparator.reverseOrder());
+
+//         int countOfCoins = 0;
+//         int amount = 583;
+//         ArrayList<Integer> ans = new ArrayList<>();
+
+//         for(int i=0; i<coins.length; i++) {
+//             if(coins[i] <= amount) {
+//                 while(coins[i] <= amount) {
+//                     countOfCoins++;
+//                     ans.add(coins[i]);
+//                     amount -= coins[i];
+//                 }
+//             }
+//         }
+
+//         System.out.println("Total (min) coins used = " + countOfCoins);
+
+//         for(int i=0; i<ans.size(); i++) {
+//             System.out.print(ans.get(i) + " ");
+//         }
+//         System.out.println();
+//     }
+// }
+
+// =======================================================================================================
+// =======================================================================================================
+ 
+// // Job Sequencing Problem  : ---
+// import java.util.*;
+// public class Greedy {
+//     static class Job {
+//         int deadline;
+//         int profit;
+//         int id;
+
+//         public Job(int i, int d, int p) {
+//             id = i;
+//             deadline = d;
+//             profit = p;
+//         }
+//     }
+//     public static void main(String args[]) {
+//         int jobsInfo[][] = {{4, 20}, {1, 10}, {1, 40}, {1, 30}};
+
+//         ArrayList<Job> jobs = new ArrayList<>();
+
+//         for(int i=0; i<jobsInfo.length; i++) {
+//             jobs.add(new Job(i, jobsInfo[i][0], jobsInfo[i][1]));
+//         }
+//         Collections.sort(jobs, (obj1, obj2) -> obj2.profit-obj1.profit); //descending order
+
+//         ArrayList<Integer> seq = new ArrayList<>();
+//         int time = 0;
+//         for(int i=0; i<jobs.size(); i++) {
+//             Job curr = jobs.get(i);
+//             if(curr.deadline > time) {
+//                 seq.add(curr.id);
+//                 time++;
+//             }
+//         }
+
+//         //print seq
+//         System.out.println("Max jobs = " + seq.size());
+//         for(int i=0; i<seq.size(); i++) {
+//             System.out.print(seq.get(i) + " ");
+//         }
+//         System.out.println();
+//     }
+// }
+
+// =======================================================================================================
+// =======================================================================================================
+ 
+// Chocola Problem : ---
+import java.util.*;
+public class Greedy {
+    public static void main(String args[]) {
+        int n = 4, m = 6;
+        Integer costVer[] = {2, 1, 3, 1, 4}; //m-1
+        Integer costHor[] = {4, 1, 2}; //n-1
+
+        Arrays.sort(costVer, Collections.reverseOrder());
+        Arrays.sort(costHor, Collections.reverseOrder());
+
+        int h = 0, v = 0;
+        int hp = 1, vp = 1;
+        int cost = 0;
+
+        while(h < costHor.length && v < costVer.length) {
+            if(costVer[v] <= costHor[h]) {  //horizonal cut
+                cost += (costHor[h] * vp);
+                hp++;
+                h++;
+            } else {  //vertical cut
+                cost += (costVer[v] * hp);
+                vp++;
+                v++;
+            }
+        }
+
+        while(h < costHor.length) {
+            cost += (costHor[h] * vp);
+            hp++;
+            h++;
+        }
+
+        while(v < costVer.length) {
+            cost += (costVer[v] * hp);
+            vp++;
+            v++;
+        }
+
+        System.out.println("Min cost of cuts = " + cost);
+    }
+}
